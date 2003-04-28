3b3330b312181827763d912fa3e213772dccb827
christi <christi@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-04-28 14:21:20 +0000

    Interface for the stubs
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@39 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ solver/pmgsolver/stubs.hh
@@ -0,0 +1,18 @@
+#ifndef STUBS_H
+#define STUBS_H
+
+/* Forward declaration of the loopstubs */
+namespace Dune {
+  namespace PMGStubs {
+    enum typ { Inner, Border };
+    
+    template <class GRID, int SMOOTHER, int TYP> class GaussSeidel;
+    template <class GRID, int SMOOTHER, int TYP> class Defect;
+    template <class GRID, int SMOOTHER, int TYP> class Restrict;
+    template <class GRID, int SMOOTHER, int TYP> class Prolongate;
+    template <class GRID, int SMOOTHER> class InitExchange;
+    template <class GRID> class InitIterator;
+  }
+}
+
+#endif // STUBS_H
