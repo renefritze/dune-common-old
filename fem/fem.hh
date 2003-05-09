d58d842ccbb9e6106594b91804a63a6145ba7a46
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-05-09 11:55:53 +0000

    Erste Version einer diskreten Function mit Funktionenraum und Basisfunktionen fuer constante, lineare und quadratische Element auf einem Dreiecksgitter. Es fehlen noch die Gradienten der Basisfunktionen. Reine Formsache. Um das ganze zu verwenden, sollte
    fem/fem.hh
    
    eingebunden werden.
    Um eine diskrete Funktion zu erzeugen, legt man einen Funktionenraum an, so
    
    FunctionSpace< Grid, Const > f; oder FunctionSpace < Grid, LagrangeOne > f;
    
    danach die Funktion
    
    DiscreteFunction < FunctionSpace < Grid , LagrangeOne > > func(f);
    
    usw.
    
    Den Rest erklaere ich per mail an die mailing Liste. Das ganze funktioniert
    bisher nur mit Dreiecksgittern in 2D, ist aber kein Problem auf 3D zu
    erweitern. Nur Arbeit, die bis zum Treffen erledigt ist.
    
    Noch soviel zu den Aufgaben der Klassen:
    
    LocalBase... Locale Basen sowie Schnittstelle fuer lokale Basen
    
    FunctionSpace: enthaelt Basen und Mapping von Elementnummer plus lokaler
       Knoten nummer auf DOF
    
    DiscreteFunction: enthaelt DOF Vektor und eval sowie evalElement
      eval -- wertet die Funktion an einem Punkt aus und verwendet dabei die
              hierarchische Struktur unserer Gitter
      evalElement -- wertet die Funktion auf einem Element (Entity )
              mittels der Basisfunktionen aus.
    
    
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@49 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ fem/fem.hh
@@ -0,0 +1,19 @@
+#ifndef DUNE_FEM_HH
+#define DUNE_FEM_HH
+
+#include "../common/matvec.hh"
+#include "../grid/common/grid.hh"
+
+#include "localbase/defaultmmgr.hh"
+#include "localbase/scalarblas.hh"
+
+
+#include "localbase.hh"
+#include "functionspace.hh"
+#include "discretefunction.hh"
+
+#include "localbase/functionspace.cc"
+#include "localbase/discretefunction.cc"
+
+#endif
+
