58fe03b3ff56d714d684ecd5889a20e6c22e36b8
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-07-24 15:45:46 +0000

    Gehoeren noch zu den Schnittstellen, hatte ich nur vergessen.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@93 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ common/function.hh
@@ -0,0 +1,33 @@
+#ifndef __DUNE_FUNCTION_HH__
+#define __DUNE_FUNCTION_HH__
+
+#include "mapping.hh"
+
+
+namespace Dune{
+
+
+template< class FunctionSpaceType, class FunctionImp>
+class Function : Mapping <typename FunctionSpaceType::RangeField , typename FunctionSpaceType::Domain, typename FunctionSpaceType::Range > {
+
+public:
+  typedef typename FunctionSpaceType::Domain Domain ;
+  typedef typename FunctionSpaceType::Range Range ;
+  typedef typename FunctionSpaceType::GradientRange GradientRange;
+  typedef typename FunctionSpaceType::HessianRange  HessianRange;
+  typedef FunctionSpaceType FunctionSpace;
+
+  Function ( const FunctionSpaceType & f ) : functionSpace_ (f) {} ;  
+
+  void evaluate ( const Domain & , Range &) const ;
+
+protected:
+  
+  const FunctionSpaceType & functionSpace_;
+
+};
+
+
+}
+
+#endif
---
+++ common/functionspace.hh
@@ -0,0 +1,28 @@
+#ifndef __DUNE_FUNCTIONSPACE_HH__
+#define __DUNE_FUNCTIONSPACE_HH__
+
+#include "matvec.hh"
+
+namespace Dune{
+
+template< typename DomainFieldType, typename RangeFieldType, int n, int m >
+class FunctionSpace : public DynamicType {
+public:
+
+  typedef DomainFieldType DomainField ;
+  typedef RangeFieldType RangeField ;
+  typedef Mat < n, m, RangeField> GradientRange;
+  typedef Vec < m,  Mat< n, n, RangeField> > HessianRange ;
+
+  typedef Vec<n, DomainField> Domain;
+  typedef Vec<m, RangeField> Range;
+
+  enum { DimDomain = n, DimRange = m};
+
+  FunctionSpace ( int ident ) : DynamicType (ident){} ;
+
+};
+
+}
+
+#endif
