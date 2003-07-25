3032bcec5d75e8bc38dbf4bd0f2040efd7682ba9
christi <christi@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-07-25 12:50:03 +0000

    exchange is const member
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@100 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/spgrid/spgrid.cc
+++ grid/spgrid/spgrid.cc
@@ -290,7 +290,7 @@ namespace Dune {
   ////////////////////////////////////////////////////////////////////////////
 
   template <int DIM>
-  void spgrid<DIM>::exchange(level l, Vector< spgrid<DIM> > & ex) {
+  void spgrid<DIM>::exchange(level l, Vector< spgrid<DIM> > & ex) const {
 //    TIME_EX -= MPI_Wtime(); 
     for (int d=0; d<DIM; d++) {
       /* every Process who's d'th coord is even */
@@ -316,7 +316,7 @@ namespace Dune {
   */
   template <int DIM>
   void spgrid<DIM>::Send(int dir, Dune::side s,
-                         level l, Vector< spgrid<DIM> > & ex) {
+                         level l, Vector< spgrid<DIM> > & ex) const {
     /* Calc the slab which we need to send */
     int sz = 1;    
     array<DIM> begin;
@@ -367,7 +367,8 @@ namespace Dune {
   */
   template <int DIM>
   void spgrid<DIM>::Recv(int dir, Dune::side s,
-                         level l, Vector< spgrid<DIM> > & ex) {
+                         level l, Vector< spgrid<DIM> > & ex) const {
+    MPI_Status mpi_status;
     /* Calc the slab which we need to send */
     int sz = 1;    
     array<DIM> begin;
