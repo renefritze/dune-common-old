cceb4534aca4b457d5e2e9e65aa958a5f83398b5
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-10-30 17:40:35 +0000

    Added SimpleGrid_id as GridIdentifie.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@220 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/common/grid.hh
+++ grid/common/grid.hh
@@ -42,7 +42,7 @@ namespace Dune {
 enum ElementType {unknown,vertex,line, triangle, quadrilateral, tetrahedron, pyramid, prism, hexahedron,
                   iso_triangle, iso_quadrilateral};
 
-enum GridIdentifier { SGrid_Id, AlbertGrid_Id };
+enum GridIdentifier { SGrid_Id, AlbertGrid_Id , SimpleGrid_id };
 
 enum FileFormatType { ascii , xdr , USPM };
 
@@ -188,7 +188,13 @@ public:
   //! define type used for coordinates in grid module
   typedef ct ctype;
 
-
+  //! calculates the bary center of the element 
+  Vec<dimworld,ctype>& barycenter ();
+  
+protected:
+  //! bary center of the element 
+  Vec<dimworld,ctype> baryCenter_; 
+  
 private:
   //! Barton-Nackman trick 
   ElementImp<dim,dimworld>& asImp () {return static_cast<ElementImp<dim,dimworld>&>(*this);}
