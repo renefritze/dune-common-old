d9dbb6cb4ab126fb7e69a020cf855bf0cebec667
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2003-11-21 09:58:31 +0000

    added a method realloc, which does exactly the same as resize. I did this because some ccfv solver expected it.  We should settle for one name eventually and adjust everything accordingly.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@269 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/array.hh
+++ common/array.hh
@@ -24,63 +24,63 @@ namespace Dune
   template<class T, int n>
   class FixedArray {
   public:
-	//! create empty array
-	FixedArray () {}
+        //! create empty array
+        FixedArray () {}
 
-	//! initialize all components with same size
-	FixedArray (T t)
-	{
-	  for (int i=0; i<n; i++) a[i]=t;
-	}
+        //! initialize all components with same size
+        FixedArray (T t)
+        {
+          for (int i=0; i<n; i++) a[i]=t;
+        }
 
-	//! assign value to all entries
-	FixedArray<T,n>& operator= (const T& t)
-	{
-	  for (int i=0; i<n; i++) a[i]=t;
-	  return (*this);
-	}
-	
-	//! component access
-	T& operator[] (int i)
-	{
-	  return a[i];
-	}
+        //! assign value to all entries
+        FixedArray<T,n>& operator= (const T& t)
+        {
+          for (int i=0; i<n; i++) a[i]=t;
+          return (*this);
+        }
+        
+        //! component access
+        T& operator[] (int i)
+        {
+          return a[i];
+        }
 
         //! const component access
         const T& operator[] (int i) const
-	{
-	  return a[i];
-	}
+        {
+          return a[i];
+        }
 
-	FixedArray<T,n-1> shrink (int comp)
-	{
-	  FixedArray<T,n-1> x;
-	  for (int i=0; i<comp; i++) x[i] = a[i];
-	  for (int i=comp+1; i<n; i++) x[i-1] = a[i];
-	  return x;
-	}
+        FixedArray<T,n-1> shrink (int comp)
+        {
+          FixedArray<T,n-1> x;
+          for (int i=0; i<comp; i++) x[i] = a[i];
+          for (int i=comp+1; i<n; i++) x[i-1] = a[i];
+          return x;
+        }
  
-	FixedArray<T,n+1> expand (int comp, T value)
-	{
-	  FixedArray<T,n+1> x;
-	  for (int i=0; i<comp; i++) x[i] = a[i];
-	  x[comp] = value;
-	  for (int i=comp+1; i<n+1; i++) x[i] = a[i-1];
-	  return x;
-	}
+        FixedArray<T,n+1> expand (int comp, T value)
+        {
+          FixedArray<T,n+1> x;
+          for (int i=0; i<comp; i++) x[i] = a[i];
+          x[comp] = value;
+          for (int i=comp+1; i<n+1; i++) x[i] = a[i-1];
+          return x;
+        }
  
   private:
-	T a[n];
+        T a[n];
   };
 
   //! Output operator for FixedArray
   template <class T, int n>
   inline std::ostream& operator<< (std::ostream& s, FixedArray<T,n> e)
   {
-	s << "[";
-	for (int i=0; i<n-1; i++) s << e[i] << ",";
-	s << e[n-1] << "]";
-	return s;
+        s << "[";
+        for (int i=0; i<n-1; i++) s << e[i] << ",";
+        s << e[n-1] << "]";
+        return s;
   }
   
   //! a simple dynamic array class with copy semantics
@@ -88,102 +88,105 @@ namespace Dune
   class Array {
   public:
   
-	//! an Iterator to access all components of array
-	class Iterator {  // Iteratorklasse zum
-	private:          // Durchlaufen der Elemente des Containers
-	  T* p;         // Iterator ist Zeiger auf Feldelement
-	public:           // Iterator ohne Bereichstest !
-	  //! make iterator  
-	  Iterator();
+        //! an Iterator to access all components of array
+        class Iterator {  // Iteratorklasse zum
+        private:          // Durchlaufen der Elemente des Containers
+          T* p;         // Iterator ist Zeiger auf Feldelement
+        public:           // Iterator ohne Bereichstest !
+          //! make iterator  
+          Iterator();
  
-	  //! equality of two iterators
-	  bool operator!= (Iterator x);
+          //! equality of two iterators
+          bool operator!= (Iterator x);
 
-	  //! inequality of two iterators
-	  bool operator== (Iterator x);
+          //! inequality of two iterators
+          bool operator== (Iterator x);
 
-	  //! prefix increment
-	  Iterator operator++ ();   // prefix Stroustrup p. 292
-	  
-	  //! postfix increment
-	  Iterator operator++ (int);// postfix
+          //! prefix increment
+          Iterator operator++ ();   // prefix Stroustrup p. 292
+          
+          //! postfix increment
+          Iterator operator++ (int);// postfix
 
-	  //! dereferencing 
-	  T& operator* () const;
+          //! dereferencing 
+          T& operator* () const;
 
-	  //! selector
-	  T* operator-> () const; // Stroustrup p. 289
+          //! selector
+          T* operator-> () const; // Stroustrup p. 289
 
-	  friend class Array<T>;
-	} ;
+          friend class Array<T>;
+        } ;
 
-	//! return iterator refering to first element in array
-	Iterator begin () const; 
+        //! return iterator refering to first element in array
+        Iterator begin () const; 
 
-	//! return iterator refering to on past the last element of the array
-	Iterator end () const;
+        //! return iterator refering to on past the last element of the array
+        Iterator end () const;
 
-	//! make empty array
-	Array();
-	
-	//! make array with m components
-	Array(int m);
+        //! make empty array
+        Array();
+        
+        //! make array with m components
+        Array(int m);
 
-	//! copy constructor making shallow copy
-	Array (const Array<T>&);
+        //! copy constructor making shallow copy
+        Array (const Array<T>&);
 
-	//! destructor
-	~Array();
+        //! destructor
+        ~Array();
 
-	//! assignment of two arrays
-	Array<T>& operator= (const Array<T>&);
+        //! assignment of two arrays
+        Array<T>& operator= (const Array<T>&);
 
-	//! assign value to all components
-	Array<T>& operator= (const T t);
+        //! assign value to all components
+        Array<T>& operator= (const T t);
 
-	//! reallocate array with size m
-	void resize (int m);
+        //! reallocate array with size m
+        void resize (int m);
 
-	//! return number of components in array
-	int size () const;
+      /** \todo This the same as resize! */
+      void realloc (int m) {resize(m);}
 
-	//! random access operator
-	T& operator[](int i);
+        //! return number of components in array
+        int size () const;
+
+        //! random access operator
+        T& operator[](int i);
         const T& operator[](int i) const;
 
-	//! export base type of array
-	typedef T MemberType;  // Merke den Grundtyp ...
+        //! export base type of array
+        typedef T MemberType;  // Merke den Grundtyp ...
 
-	//! print contents of array, k elements in a row
-	void print (int k, std::string s, std::string row)
-	{
-	  char buf[96];
-	  std::cout << s << " size=" << n << " {" << std::endl;
-	  for (int i=0; i<n/k; i++)
-		{
-		  sprintf(buf,"%4d",k*i);
-		  std::cout << buf << " " << row << " ";
-		  for (int j=i*k; j<(i+1)*k; j++)
-			{
-			  sprintf(buf,"%10.3E ",p[j]);
-			  std::cout << buf;
-			}
-		  std::cout << std::endl;
-		}
-	  int i = (n/k)*k;
-	  if (i<n)
-		{
-		  sprintf(buf,"%4d",k*i);
-		  std::cout << buf << " " << row << " ";
-		  for (int j=i; j<n; j++)
-			{
-			  sprintf(buf,"%10.3E ",p[j]);
-			  std::cout << buf;
-			}
-		  std::cout << std::endl;
-		}
-	  std::cout << "}" << std::endl;	  
-	}
+        //! print contents of array, k elements in a row
+        void print (int k, std::string s, std::string row)
+        {
+          char buf[96];
+          std::cout << s << " size=" << n << " {" << std::endl;
+          for (int i=0; i<n/k; i++)
+                {
+                  sprintf(buf,"%4d",k*i);
+                  std::cout << buf << " " << row << " ";
+                  for (int j=i*k; j<(i+1)*k; j++)
+                        {
+                          sprintf(buf,"%10.3E ",p[j]);
+                          std::cout << buf;
+                        }
+                  std::cout << std::endl;
+                }
+          int i = (n/k)*k;
+          if (i<n)
+                {
+                  sprintf(buf,"%4d",k*i);
+                  std::cout << buf << " " << row << " ";
+                  for (int j=i; j<n; j++)
+                        {
+                          sprintf(buf,"%10.3E ",p[j]);
+                          std::cout << buf;
+                        }
+                  std::cout << std::endl;
+                }
+          std::cout << "}" << std::endl;          
+        }
 
   bool processXdr(XDR *xdrs)
   {
@@ -197,8 +200,8 @@ namespace Dune
   }
 
   protected:
-	int n;  // Anzahl Elemente; n=0 heisst, dass kein array allokiert ist!
-	T *p;   // Zeiger auf built-in array
+        int n;  // Anzahl Elemente; n=0 heisst, dass kein array allokiert ist!
+        T *p;   // Zeiger auf built-in array
   } ;
 
 
@@ -206,232 +209,232 @@ namespace Dune
   template<class T>
   inline typename Array<T>::Iterator Array<T>::begin () const
   {
-	Iterator tmp;  // hat Zeiger 0
-	tmp.p = p;     // Zeiger auf Feldelement 0
-	return tmp;
+        Iterator tmp;  // hat Zeiger 0
+        tmp.p = p;     // Zeiger auf Feldelement 0
+        return tmp;
   }
   
   template<class T>
   inline typename Array<T>::Iterator Array<T>::end () const
   {
-	Iterator tmp;
-	tmp.p = &(p[n]); // Zeiger auf Feldelement NACH letztem !
-	return tmp;      // das funktioniert: Stroustrup p. 92.
+        Iterator tmp;
+        tmp.p = &(p[n]); // Zeiger auf Feldelement NACH letztem !
+        return tmp;      // das funktioniert: Stroustrup p. 92.
   }
 
   // Destruktor
   template <class T>
   inline Array<T>::~Array () 
   { 
-	if (n>0) delete[] p; 
+        if (n>0) delete[] p; 
   }
   
   // Konstruktoren
   template <class T>
   inline Array<T>::Array ()
   {
-	n = 0;
+        n = 0;
   }
   
   template <class T>
   inline Array<T>::Array (int m)
   {
-	n = m;
-	if (n<=0)
-	  {
-		n = 0;
-		return;
-	  }
-	try {
-	  p = new T[n];
-	}
-	catch (std::bad_alloc) {
-	  std::cerr << "nicht genug Speicher!" << std::endl;
-	  throw;
-	}
+        n = m;
+        if (n<=0)
+          {
+                n = 0;
+                return;
+          }
+        try {
+          p = new T[n];
+        }
+        catch (std::bad_alloc) {
+          std::cerr << "nicht genug Speicher!" << std::endl;
+          throw;
+        }
   }
 
   template <class T>
   inline void Array<T>::resize (int m)
   {
-	if (m!=n)
-	  {
-		if (n>0) delete[] p;
-		n = m;
-		if (n==0) return;
-		try {
-		  p = new T[n];
-		}
-		catch (std::bad_alloc) {
-		  std::cerr << "nicht genug Speicher!" << std::endl;
-		  throw;
-		}
-	  }
+        if (m!=n)
+          {
+                if (n>0) delete[] p;
+                n = m;
+                if (n==0) return;
+                try {
+                  p = new T[n];
+                }
+                catch (std::bad_alloc) {
+                  std::cerr << "nicht genug Speicher!" << std::endl;
+                  throw;
+                }
+          }
   }
-  
+
   // Copy-Konstruktor
   template <class T>
   inline Array<T>::Array (const Array<T>& a)
   {
-	// Erzeuge Feld mit selber Groesse wie a
-	n = a.n;
-	if (n>0)
-	  {
-		try {
-		  p = new T[n];
-		}
-		catch (std::bad_alloc) {
-		  std::cerr << "nicht genug Speicher!" << std::endl;
-		  throw;
-		}
-	  }
+        // Erzeuge Feld mit selber Groesse wie a
+        n = a.n;
+        if (n>0)
+          {
+                try {
+                  p = new T[n];
+                }
+                catch (std::bad_alloc) {
+                  std::cerr << "nicht genug Speicher!" << std::endl;
+                  throw;
+                }
+          }
 
-	// und kopiere Elemente
-	for (int i=0; i<n; i=i+1) p[i]=a.p[i];
+        // und kopiere Elemente
+        for (int i=0; i<n; i=i+1) p[i]=a.p[i];
   }
 
   // Zuweisung
   template <class T>
   inline Array<T>& Array<T>::operator= (const Array<T>& a)
   {
-	if (&a!=this) // nur bei verschiedenen Objekten was tun
-	  {
-		if (n!=a.n)
-		  {
-			// allokiere fuer this ein Feld der Groesse a.n
-			if (n>0) delete[] p; // altes Feld loeschen
-			n = a.n;
-			if (n>0)
-			  {
-				try {
-				  p = new T[n];
-				}
-				catch (std::bad_alloc) {
-				  std::cerr << "nicht genug Speicher!" << std::endl;
-				  throw;
-				}
-			  }
-		  }
-		for (int i=0; i<n; i++) p[i]=a.p[i];
-	  }
-	return *this; // Gebe Referenz zurueck damit a=b=c; klappt
+        if (&a!=this) // nur bei verschiedenen Objekten was tun
+          {
+                if (n!=a.n)
+                  {
+                        // allokiere fuer this ein Feld der Groesse a.n
+                        if (n>0) delete[] p; // altes Feld loeschen
+                        n = a.n;
+                        if (n>0)
+                          {
+                                try {
+                                  p = new T[n];
+                                }
+                                catch (std::bad_alloc) {
+                                  std::cerr << "nicht genug Speicher!" << std::endl;
+                                  throw;
+                                }
+                          }
+                  }
+                for (int i=0; i<n; i++) p[i]=a.p[i];
+          }
+        return *this; // Gebe Referenz zurueck damit a=b=c; klappt
   }
 
   // Zuweisung mit member type
   template <class T>
   inline Array<T>& Array<T>::operator= (T a)
   {
-	for (int i=0; i<n; i++) p[i]=a;
-	return *this; // Gebe Referenz zurueck damit a=b=c; klappt
+        for (int i=0; i<n; i++) p[i]=a;
+        return *this; // Gebe Referenz zurueck damit a=b=c; klappt
   }
   
   // Indizierung
   template <class T>
   inline T& Array<T>::operator[] (int i)
   {
-	return p[i];
+        return p[i];
   }
   template <class T>
   inline const T& Array<T>::operator[] (int i) const
   {
-	return p[i];
+        return p[i];
   }
 
   // Groesse
   template <class T>
   inline int Array<T>::size () const
   { 
-	return n; 
+        return n; 
   }
 
   // Ausgabe
   template <class T>
   std::ostream& operator<< (std::ostream& s, Array<T>& a)
   {
-	s << "array " << a.size() << " elements = [" << std::endl;
-	for (int i=0; i<a.size(); i++)
-	  s << "    " << i << "  " << a[i] << std::endl;
-	s << "]" << std::endl;
-	return s;
+        s << "array " << a.size() << " elements = [" << std::endl;
+        for (int i=0; i<a.size(); i++)
+          s << "    " << i << "  " << a[i] << std::endl;
+        s << "]" << std::endl;
+        return s;
   }
 
   template<class T>
   inline Array<T>::Iterator::Iterator () 
   {
-	p=0; // nicht initialisierter Iterator
+        p=0; // nicht initialisierter Iterator
   }
 
   template<class T>
   inline bool Array<T>::Iterator::operator!= 
   (Array<T>::Iterator x)
   {
-	return p != x.p;
+        return p != x.p;
   }
 
   template<class T>
   inline bool Array<T>::Iterator::operator== 
   (Array::Iterator x)
   {
-	return p == x.p;
+        return p == x.p;
   }
-		
+                
   template<class T>
   inline typename Array<T>::Iterator Array<T>::Iterator::operator++ () // prefix
   {
-	p++;  // C Zeigerarithmetik: p zeigt auf naechstes Feldelement
-	return *this;
+        p++;  // C Zeigerarithmetik: p zeigt auf naechstes Feldelement
+        return *this;
   }
 
   template<class T>
   inline typename Array<T>::Iterator Array<T>::Iterator::operator++ (int) // postfix
   {
-	Iterator tmp = *this;
-	++*this;
-	return tmp;
+        Iterator tmp = *this;
+        ++*this;
+        return tmp;
   }
 
   template<class T>
   inline T& Array<T>::Iterator::operator* () const
   {
-	return *p;
+        return *p;
   }
-		
+                
   template<class T>
   inline T* Array<T>::Iterator::operator-> () const
   {
-	return p;
+        return p;
   }
 
   //! a simple vector class derived from array
   template <class T> 
   class SimpleVector : public Array<T> {
   public:
-	//! make empty vector
-	SimpleVector() {};
-	
-	//! make array with m components
-	SimpleVector(int m) : Array<T>::Array(m) {}
+        //! make empty vector
+        SimpleVector() {};
+        
+        //! make array with m components
+        SimpleVector(int m) : Array<T>::Array(m) {}
 
-	//! assignment from scalar
-	SimpleVector<T>& operator= (const T t)
-	{
-	  for (int i=0; i<n; ++i) p[i] = t;
-	  return *this;
-	}
+        //! assignment from scalar
+        SimpleVector<T>& operator= (const T t)
+        {
+          for (int i=0; i<n; ++i) p[i] = t;
+          return *this;
+        }
 
-	//! scalar product of two vectors, no check for size !
-	T ddot (const SimpleVector<T>& x)
-	{
-	  T sum = 0;
-	  for (int i=0; i<n; ++i) sum += p[i]*x.p[i];
-	  return sum;
-	}
+        //! scalar product of two vectors, no check for size !
+        T ddot (const SimpleVector<T>& x)
+        {
+          T sum = 0;
+          for (int i=0; i<n; ++i) sum += p[i]*x.p[i];
+          return sum;
+        }
 
-	// add scalar times other vector
-	void daxpy (T a, const SimpleVector<T>& x)
-	{
-	  for (int i=0; i<n; ++i) p[i] += a*x.p[i];
-	}
+        // add scalar times other vector
+        void daxpy (T a, const SimpleVector<T>& x)
+        {
+          for (int i=0; i<n; ++i) p[i] += a*x.p[i];
+        }
   };
   
   /** @} */
