9becb7bbdc1997801221b502d9a78697776bac94
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-03-02 11:11:39 +0000

    all UG-includes separated into a designated header file
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@355 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ grid/uggrid/ugincludes.hh
@@ -0,0 +1,19 @@
+// All UG includes have to be made from this file, and from
+// this file only!  This is because undefAllMacros.pl takes
+// all headers from this file and undefs the macros defined
+// therein.
+
+#ifndef DUNE_UGINCLUDES_HH
+#define DUNE_UGINCLUDES_HH
+
+#include "../../../UG/ug/gm/gm.h"
+#include "../../../UG/ug/dom/std/std_domain.h"
+#include "../../../UG/ug/initug.h"
+#include "../../../UG/ug/ui/commands.h"
+#include "../../../UG/ug/np/udm/formats.h"
+
+
+// for debugging
+#include "../../../UG/ug/dev/ugdevices.h"
+
+#endif
