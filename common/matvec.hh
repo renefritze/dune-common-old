0ee37de03fa8df5754b22a89d3a32911c8621870
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-03-29 13:21:54 +0000

    Added the method mult_t on mat, which multiplies a Vec with the transpose of the matrix, if the matrix is symetric.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@417 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/matvec.hh
+++ common/matvec.hh
@@ -9,10 +9,10 @@
 namespace Dune {
 /** @defgroup Common Dune Common Module
 
-	This module contains classes for general usage in dune, such as e.g.
-	(small) dense matrices and vectors or containers.
+  This module contains classes for general usage in dune, such as e.g.
+  (small) dense matrices and vectors or containers.
 
-	@{
+  @{
  */
 
 
@@ -33,161 +33,161 @@ public:
   //! know length 
   enum { dimension = dim };
   
-	//! Constructor making uninizialized vector
-	Vec() {}
+  //! Constructor making uninizialized vector
+  Vec() {}
 
-	//! Constructor making vector from built-in array
-	Vec (T* y) {for(int i=0; i<n; i++) x[i]=y[i];}
+  //! Constructor making vector from built-in array
+  Vec (T* y) {for(int i=0; i<n; i++) x[i]=y[i];}
 
-	//! Constructor making vector with one coordinate set, others zeroed
-	Vec (int k, T t)
-	{
-		for (int i=0; i<n; i++) x[i] = 0; 
-		x[k] = t;
-	}
+  //! Constructor making vector with one coordinate set, others zeroed
+  Vec (int k, T t)
+  {
+    for (int i=0; i<n; i++) x[i] = 0; 
+    x[k] = t;
+  }
  
-	//! Constructor making vector with identical coordinates
-	Vec (T t)
-	{
-		for (int i=0; i<n; i++) x[i] = t;
-	}
+  //! Constructor making vector with identical coordinates
+  Vec (T t)
+  {
+    for (int i=0; i<n; i++) x[i] = t;
+  }
  
-	//! assign component type to all components
-	Vec<n,T>& operator= (const Vec<n,T>& b)
-	{
-		for (int i=0; i<n; i++) x[i] = b.x[i]; 
-		return *this;
-	}
+  //! assign component type to all components
+  Vec<n,T>& operator= (const Vec<n,T>& b)
+  {
+    for (int i=0; i<n; i++) x[i] = b.x[i]; 
+    return *this;
+  }
 
-	//! assign component type to all components
-	Vec<n,T>& operator= (T t)
-	{
-		for (int i=0; i<n; i++) x[i]=t;
-		return *this;
-	}
-	Vec<n,T>& operator= (T* t)
-	{
-		for (int i=0; i<n; i++) x[i]=t[i];
-		return *this;
-	}
+  //! assign component type to all components
+  Vec<n,T>& operator= (T t)
+  {
+    for (int i=0; i<n; i++) x[i]=t;
+    return *this;
+  }
+  Vec<n,T>& operator= (T* t)
+  {
+    for (int i=0; i<n; i++) x[i]=t[i];
+    return *this;
+  }
 
-	//! operator () for read/write access to element of the vector
-	T& operator() (int i) {return x[i];}
+  //! operator () for read/write access to element of the vector
+  T& operator() (int i) {return x[i];}
 
-	//! operator () for read access to element of the vector
-	const T& operator() (int i) const {return x[i];}
+  //! operator () for read access to element of the vector
+  const T& operator() (int i) const {return x[i];}
 
-	//! read only operation needed
-	T read (int i) const { return x[i];}
+  //! read only operation needed
+  T read (int i) const { return x[i];}
 
-	//! operator+ adds two vectors
-	Vec<n,T>& operator+= (const Vec<n,T>& b)
-	{
-		for (int i=0; i<n; i++) x[i] += b.x[i]; 
-		return *this;
-	}
-	Vec<n,T> operator+ (const Vec<n,T>& b) const
-	{
-		Vec<n,T> z = *this; 
-		return (z+=b);
-	}
+  //! operator+ adds two vectors
+  Vec<n,T>& operator+= (const Vec<n,T>& b)
+  {
+    for (int i=0; i<n; i++) x[i] += b.x[i]; 
+    return *this;
+  }
+  Vec<n,T> operator+ (const Vec<n,T>& b) const
+  {
+    Vec<n,T> z = *this; 
+    return (z+=b);
+  }
 
-	//! operator- binary minus
-	Vec<n,T>& operator-= (const Vec<n,T>& b)
-	{
-		for (int i=0; i<n; i++) x[i] -= b.x[i]; 
-		return *this;
-	}
-	Vec<n,T> operator- (const Vec<n,T>& b) const
-	{
-		Vec<n,T> z = *this; 
-		return (z-=b);
-	}
+  //! operator- binary minus
+  Vec<n,T>& operator-= (const Vec<n,T>& b)
+  {
+    for (int i=0; i<n; i++) x[i] -= b.x[i]; 
+    return *this;
+  }
+  Vec<n,T> operator- (const Vec<n,T>& b) const
+  {
+    Vec<n,T> z = *this; 
+    return (z-=b);
+  }
 
-	//! scalar product of two vectors with operator* 
-	T operator* (const Vec<n,T>& b) const
-	{
-		T s=0; for (int i=0; i<n; i++) s += x[i]*b.x[i]; return s;
-	}
+  //! scalar product of two vectors with operator* 
+  T operator* (const Vec<n,T>& b) const
+  {
+    T s=0; for (int i=0; i<n; i++) s += x[i]*b.x[i]; return s;
+  }
 
-	//! multiplication of vector with scalar
-	T operator* (T k) const
-	{
-		Vec<n,T> z; for (int i=0; i<n; i++) z.x[i] =  k*x[i]; return z;
-	}
+  //! multiplication of vector with scalar
+  T operator* (T k) const
+  {
+    Vec<n,T> z; for (int i=0; i<n; i++) z.x[i] =  k*x[i]; return z;
+  }
 
-	//! multiplication assignment with scalar
-	Vec<n,T>& operator*= (T k)
-	{
-		for (int i=0; i<n; i++) x[i] *= k; 
-		return *this;
-	}
+  //! multiplication assignment with scalar
+  Vec<n,T>& operator*= (T k)
+  {
+    for (int i=0; i<n; i++) x[i] *= k; 
+    return *this;
+  }
 
-	//! 1 norm
-	T norm1 () const
-	{
-		T s=0.0;
-		for (int i=0; i<n; i++) s += ABS(x[i]);
-		return s;
-	}
+  //! 1 norm
+  T norm1 () const
+  {
+    T s=0.0;
+    for (int i=0; i<n; i++) s += ABS(x[i]);
+    return s;
+  }
 
-	//! 2 norm
-	T norm2 () const
-	{
-		T s=0.0;
-		for (int i=0; i<n; i++) s += x[i]*x[i];
-		return sqrt(s);
-	}
+  //! 2 norm
+  T norm2 () const
+  {
+    T s=0.0;
+    for (int i=0; i<n; i++) s += x[i]*x[i];
+    return sqrt(s);
+  }
 
-	//! \infty norm
-	T norminfty () const
-	{
-		T s=0.0;
-		for (int i=0; i<n; i++)
-			if (ABS(x[i])>s) s = ABS(x[i]);
-		return s;
-	}
+  //! \infty norm
+  T norminfty () const
+  {
+    T s=0.0;
+    for (int i=0; i<n; i++)
+      if (ABS(x[i])>s) s = ABS(x[i]);
+    return s;
+  }
 
-	//! Eucledian distance of two vectors
-	T distance (const Vec<n,T>& b) const
-	{
-		T s=0.0;
-		for (int i=0; i<n; i++) s += (x[i]-b.x[i])*(x[i]-b.x[i]);
-		return sqrt(s);
-	}
+  //! Eucledian distance of two vectors
+  T distance (const Vec<n,T>& b) const
+  {
+    T s=0.0;
+    for (int i=0; i<n; i++) s += (x[i]-b.x[i])*(x[i]-b.x[i]);
+    return sqrt(s);
+  }
 
-	void print (std::ostream& s, int indent) const
-	{
-		for (int k=0; k<indent; k++) s << " ";
-		s << "Vec [ ";
-		for (int i=0; i<n; i++) s << x[i] << " ";
-		s << "]";
-	}
+  void print (std::ostream& s, int indent) const
+  {
+    for (int k=0; k<indent; k++) s << " ";
+    s << "Vec [ ";
+    for (int i=0; i<n; i++) s << x[i] << " ";
+    s << "]";
+  }
 
 private:
-	//! built-in array to hold the data.
-	T x[n];
+  //! built-in array to hold the data.
+  T x[n];
 };
 
 //! multiplication of scalar with vector
 template<int n, class T> 
 inline Vec<n,T> operator* (T k, Vec<n,T> b)
 {
-	Vec<n,T> z; for (int i=0; i<n; i++) z(i) = k*b(i); return z;
+  Vec<n,T> z; for (int i=0; i<n; i++) z(i) = k*b(i); return z;
 }
 
 //! unary minus
 template<int n, class T> 
 inline Vec<n,T> operator- (Vec<n,T> b)
 {
-	Vec<n,T> z; for (int i=0; i<n; i++) z(i) = -b(i); return z;
+  Vec<n,T> z; for (int i=0; i<n; i++) z(i) = -b(i); return z;
 }
 
 template <int n, class T>
-inline std::ostream& operator<< (std::ostream& s, Vec<n,T>& v)
+inline std::ostream& operator<< (std::ostream& s, const Vec<n,T>& v)
 {
-	v.print(s,0);
-	return s;
+  v.print(s,0);
+  return s;
 }
 
 //************************************************************************
@@ -203,53 +203,63 @@ public:
   enum { dimRow = dim };
   enum { dimCol = n };
   
-	//! Constructor making uninizialized matrix
-	Mat() {}
+  //! Constructor making uninizialized matrix
+  Mat() {}
 
   //! Constructor setting all entries to t
-	Mat(T t)
-	{
+  Mat(T t)
+  {
     for(int j=0; j<m; j++)
-		  for (int i=0; i<n; i++) a[j](i) = t;
-	}
+      for (int i=0; i<n; i++) a[j](i) = t;
+  }
 
-	//! operator () for read/write access to element in matrix
-	T& operator() (int i, int j) {return a[j](i);}
+  //! operator () for read/write access to element in matrix
+  T& operator() (int i, int j) {return a[j](i);}
   
-	//! operator () for read/write access to element in matrix
-	const T& operator() (int i, int j) const {return a[j](i);} 
+  //! operator () for read/write access to element in matrix
+  const T& operator() (int i, int j) const {return a[j](i);} 
 
-	//! operator () for read/write access to column vector
-	Vec<n,T>& operator() (int j) {return a[j];}
+  //! operator () for read/write access to column vector
+  Vec<n,T>& operator() (int j) {return a[j];}
 
-	//! matrix/vector multiplication
-	Vec<n,T> operator* (const Vec<m,T>& x)
-	{
-		Vec<n,T> z(0.0);
-		for (int j=0; j<m; j++)
-			for (int i=0; i<n; i++) z(i) += a[j](i) * x.read(j);
-		return z;
-	}
+  //! matrix/vector multiplication
+  Vec<n,T> operator* (const Vec<m,T>& x)
+  {
+    Vec<n,T> z(0.0);
+    for (int j=0; j<m; j++)
+      for (int i=0; i<n; i++) z(i) += a[j](i) * x.read(j);
+    return z;
+  }
   
-	//! multiplication assignment with scalar
-	Mat<n,m,T>& operator*= (T t)
-	{
+  //! matrix/vector multiplication with transpose of matrix 
+  Vec<n,T> mult_t (const Vec<m,T>& x)
+  {
+    Vec<n,T> z(0.0);
+    const Mat<n,m,T> &matrix = (*this);
+    for (int j=0; j<m; j++)
+      for (int i=0; i<n; i++) z(i) += matrix(j,i) * x.read(j);
+    return z;
+  }
+  
+  //! multiplication assignment with scalar
+  Mat<n,m,T>& operator*= (T t)
+  {
     for(int j=0; j<m; j++) a[j] *= t; 
-		return *this;
-	}
+    return *this;
+  }
 
-	void print (std::ostream& s, int indent)
-	{
-		for (int k=0; k<indent; k++) s << " ";
-		s << "Mat [n=" << n << ",m=" << m << "]" << std::endl;
-		for (int i=0; i<n; i++)
-		{
-			for (int k=0; k<indent+2; k++) s << " ";
-			s << "row " << i << " [ ";
-			for (int j=0; j<m; j++) s << this->operator()(i,j) << " ";
-			s << "]" << std::endl;
-		}
-	}
+  void print (std::ostream& s, int indent)
+  {
+    for (int k=0; k<indent; k++) s << " ";
+    s << "Mat [n=" << n << ",m=" << m << "]" << std::endl;
+    for (int i=0; i<n; i++)
+    {
+      for (int k=0; k<indent+2; k++) s << " ";
+      s << "row " << i << " [ ";
+      for (int j=0; j<m; j++) s << this->operator()(i,j) << " ";
+      s << "]" << std::endl;
+    }
+  }
 
   //! calculates the determinant of this matrix 
   T determinant () const;
@@ -258,9 +268,13 @@ public:
   //! the parameter inverse, return is the determinant
   T invert (Mat<n,m,T>& inverse) const;
 
+  //! calculates the transpose of this matrix and stores it in 
+  //! the parameter transpose, return is the determinant
+  void transpose (Mat<m,n,T>& transpose) const;
+
 private:
-	//! built-in array to hold the data
-	Vec<n,T> a[m];
+  //! built-in array to hold the data
+  Vec<n,T> a[m];
 };
 
 namespace HelpMat {
@@ -366,8 +380,8 @@ static inline T invertMatrix (const Mat<3,3,T> &matrix, Mat<3,3,T> &inverse)
 template <int n, int m, class T>
 inline std::ostream& operator<< (std::ostream& s, Mat<n,m,T>& A)
 {
-	A.print(s,0);
-	return s;
+  A.print(s,0);
+  return s;
 }
 
 // implementation od determinant 
@@ -382,6 +396,16 @@ template <int n, int m, class T>
 inline T Mat<n,m,T>::invert (Mat<n,m,T>& inverse) const
 {
   return HelpMat::invertMatrix( *this , inverse);
+}
+
+// implementation of transpose 
+template <int n, int m, class T>
+inline void Mat<n,m,T>::transpose (Mat<m,n,T>& transpose) const
+{
+  const Mat<n,m,T> & matrix = (*this);
+  for(int i=0; i<n; i++)
+    for(int k=0; k<m; k++)
+      transpose(i,k) = matrix(k,i);
 }
 
 
