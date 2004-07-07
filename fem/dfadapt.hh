91df7b04612d5d8f62129276b6670d04915a0195
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-07-07 09:05:29 +0000

    Removed warning.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@553 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/dfadapt.hh
+++ fem/dfadapt.hh
@@ -287,8 +287,8 @@ public:
     :  dofArray_ ( dofArray ) , constArray_ (dofArray) , count_ ( count ) {};
   
   DofIteratorAdapt ( const DofArrayType & dofArray , int count )
-    :  constArray_ ( dofArray ) , 
-       dofArray_ ( const_cast <DofArrayType &> (dofArray) ) ,
+    :  dofArray_ ( const_cast <DofArrayType &> (dofArray) ) ,
+       constArray_ ( dofArray ) , 
        count_ ( count ) {};
 
   //! return dof
