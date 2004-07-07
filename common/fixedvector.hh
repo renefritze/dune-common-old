132363c7acc95ca934e22e8736000f170495dcbc
mario <mario@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-07-07 08:43:34 +0000

    return type of multiplikation with a scalar corrected
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@550 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/fixedvector.hh
+++ common/fixedvector.hh
@@ -86,7 +86,7 @@ public:
   }
 
   //! multiplication of vector with scalar
-  T operator* (T k) const
+  Vec<n,T> operator* (T k) const
   {
     Vec<n,T> z; for (int i=0; i<n; i++) z.a[i] =  k*a[i]; return z;
   }
