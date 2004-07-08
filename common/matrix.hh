cc0641e5d8f72bbcf7d54c371f93d63990c1d123
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-07-08 07:42:29 +0000

    Multiplikation mit einem Skalar
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@557 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/matrix.hh
+++ common/matrix.hh
@@ -70,6 +70,15 @@ public:
         return cols_;
     }
 
+    /** \brief Multiplication with a scalar */
+    Matrix<T> operator*=(const T& scalar) {
+        for (int row=0; row<rows_; row++) 
+            for (int col=0; col<cols_; col++)
+                (*this)(row, col) *= scalar;
+        
+        return (*this);
+    }
+
     /** \brief Return the transpose of the matrix */
     Matrix transpose() const {
         Matrix out(cols(), rows());
