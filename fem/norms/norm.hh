827107615a97055e5ef1877d89c146b82552658c
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-07-19 12:52:07 +0000

    Prototyp eines Frameworks zur Berechnung von Normen von diskreten Funktionen. Es gibt eine Basisklasse Norm<class DiscFuncType> mit einer rein virtuellen Methode compute.  Konkrete Normen leiten von dieser Klasse ab und reimplementieren compute.  Als Beispiel habe ich mal die L_2-Norm eingebaut. Verwendet wird das ganze zum Beispiel in meinem LinearSolver.
    Wie gesagt ein Prototyp.  Wer Kritik oder Anregungen hat möge sie äußern!
    
    
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@590 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ fem/norms/norm.hh
@@ -0,0 +1,19 @@
+#ifndef __DUNE_NORM_HH__
+#define __DUNE_NORM_HH__
+
+namespace Dune {
+
+    //! Abstract base for classes computing norms of discrete functions
+    template <class DiscFuncType>
+    class Norm {
+
+    public:
+
+        //! Compute the norm
+        virtual double compute(const DiscFuncType& f) = 0;
+
+    };
+
+}
+
+#endif
