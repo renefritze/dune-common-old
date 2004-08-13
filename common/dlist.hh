f75fa1ef42363aa343daf5e0e4ef9790329c6d4b
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-08-13 08:26:53 +0000

    Doxygen
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@674 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/dlist.hh
+++ common/dlist.hh
@@ -1,52 +1,103 @@
-#ifndef __DLIST_HH__
-#define __DLIST_HH__
+#ifndef __DUNE_DLIST_HH__
+#define __DUNE_DLIST_HH__
 
 namespace Dune {
 
-  template <class T> class DoubleLinkedList {
+    /** \brief A doubly-linked list
+     */
+  template <class T> 
+  class DoubleLinkedList {
   private:
-	struct Element;   // Vorwaertsdeklaration fuer das Listenelement 
+        struct Element;   // Vorwaertsdeklaration fuer das Listenelement 
   public:
-	class Iterator {  // Iteratorklasse zum
-	private:          // Durchlaufen der Elemente des Containers
-	  Element* p;   // Iterator ist ein Zeiger auf ein Listenelement
-	public:
-	  Iterator();
-	  bool operator!= (Iterator x);
-	  bool operator== (Iterator x);
-	  Iterator operator++ ();   // prefix Stroustrup p. 292
-	  Iterator operator++ (int);// postfix
-	  Iterator operator-- ();   // prefix
-	  Iterator operator-- (int);// postfix
-	  T& operator* () const;
-	  T* operator-> () const; // Stroustrup p. 289
-	  friend class DoubleLinkedList<T>;
-	} ;
-	Iterator begin () const; 
-	Iterator end () const;
-	Iterator rbegin () const; 
-	Iterator rend () const;
-	
-	DoubleLinkedList();
-	DoubleLinkedList (const DoubleLinkedList<T>&);
-	~DoubleLinkedList();
-	DoubleLinkedList<T>& operator= (const DoubleLinkedList<T>&);
-	int size () const;
-	Iterator insert_after (Iterator i, T& t);
-	Iterator insert_before (Iterator i, T& t);
-	void erase (Iterator i);
+
+      /** \brief Iterator class for the doubly-linked list
+       */
+        class Iterator {
+        private:
+            //! Iterator is a pointer to a list element
+          Element* p;
+        public:
+
+            //! ???
+          Iterator();
+
+            //! ???
+          bool operator!= (Iterator x);
+
+            //! ???
+          bool operator== (Iterator x);
+
+            //! Prefix increment
+          Iterator operator++ ();
+
+            //! Postfix increment
+          Iterator operator++ (int);
+
+            //! Prefix decrement
+          Iterator operator-- ();
+
+            //! Postfix decrement
+          Iterator operator-- (int);
+
+            //! ???
+          T& operator* () const;
+
+            //! ???
+          T* operator-> () const; // Stroustrup p. 289
+
+            //! ???
+          friend class DoubleLinkedList<T>;
+        } ;
+
+            //! ???
+        Iterator begin () const; 
+
+            //! ???
+        Iterator end () const;
+
+            //! ???
+        Iterator rbegin () const; 
+
+            //! ???
+        Iterator rend () const;
+        
+
+            //! ???
+        DoubleLinkedList();
+
+            //! ???
+        DoubleLinkedList (const DoubleLinkedList<T>&);
+
+            //! ???
+        ~DoubleLinkedList();
+
+            //! ???
+        DoubleLinkedList<T>& operator= (const DoubleLinkedList<T>&);
+
+            //! ???
+        int size () const;
+
+            //! ???
+        Iterator insert_after (Iterator i, T& t);
+
+            //! ???
+        Iterator insert_before (Iterator i, T& t);
+
+            //! ???
+        void erase (Iterator i);
 
   private:
-	struct Element {      // Typ fuer das Listenelement
-	  Element* next;    // Nachfolger
-	  Element* prev;    // Vorgaenger
-	  T item;           // Datum
-	  Element (T &t);   // setze next=prev=0
-	};
+        struct Element {      // Typ fuer das Listenelement
+          Element* next;    // Nachfolger
+          Element* prev;    // Vorgaenger
+          T item;           // Datum
+          Element (T &t);   // setze next=prev=0
+        };
 
-	Iterator head;        // erstes Element der Liste
-	Iterator tail;        // letztes Element der Liste
-	int numelements;      // Anzahl Elemente in der Liste
+        Iterator head;        // erstes Element der Liste
+        Iterator tail;        // letztes Element der Liste
+        int numelements;      // Anzahl Elemente in der Liste
   } ;
 
 }
