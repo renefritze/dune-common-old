a1782afcd453e43b74112cba2c65f8047320e198
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-09-02 11:47:19 +0000

    Minor changes for parallel runs.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@741 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/albertgrid.hh
+++ grid/albertgrid.hh
@@ -69,11 +69,27 @@ extern "C"
 #endif
 #endif
 
+
+namespace Dune {
+
+ enum IteratorType { Master  , //!< iterate over all entities which belong to
+                     Interior, //!< iterate over all interior entities
+                     Border  , //!< iterate over entities which define the pr
+                               //!< border , all codims possible             
+                     Ghosts ,  //!< iterate over all ghost cells
+                     InteriorBorder,  //!< iterate over Interior and Border
+                     All ,    //!< iterate over all cells on this processor
+                   };
+}
+
 // contains a simple memory management for some componds of this grid 
 #include "albertgrid/agmemory.hh"
 
 // contains the communication for parallel computing for this grid
+
+#ifndef MPI_ACTIVE
 #undef HAVE_MPI 
+#endif
 #include "albertgrid/agcommunicator.hh"
 
 namespace Dune 
@@ -102,16 +118,8 @@ namespace Dune
 
   @{
  */
-    enum IteratorType { Master  , //!< iterate over all entities which belong to
-                        Interior, //!< iterate over all interior entities
-                        Border  , //!< iterate over entities which define the pr
-                                  //!< border , all codims possible             
-                        Ghosts ,  //!< iterate over all ghost cells
-                        InteriorBorder,  //!< iterate over Interior and Border
-                        All ,    //!< iterate over all cells on this processor
-                      };
 
-  
+
 // i.e. double or float 
 typedef ALBERT REAL albertCtype;
 
@@ -1154,7 +1162,9 @@ public:
 
   //! calculate max edge length 
   double calcGridWidth ();
-  
+
+  // void checkBoundray();
+
 private:
   // initialize of some members 
   void initGrid(int proc);
@@ -1341,6 +1351,5 @@ private:
 
 #include "albertgrid/agmemory.hh"
 #include "albertgrid/albertgrid.cc"
-//#include "albertgrid/albertparalleliterator.cc"
 
 #endif
