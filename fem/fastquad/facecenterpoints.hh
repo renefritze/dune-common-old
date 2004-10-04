361da99f662d0375fcef3857d9e230073a542ecd
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-10-04 17:06:56 +0000

    bug in barycenter wuad for tetrahedrons codim 0.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@812 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/fastquad/facecenterpoints.hh
+++ fem/fastquad/facecenterpoints.hh
@@ -111,7 +111,7 @@ struct BaryCenterPoints<Domain,RangeField,tetrahedron,0>
   enum { polynomOrder = 1 };
   static Domain getPoint (int i) 
   {
-    Domain tmp = (1.0/3.0);
+    Domain tmp = (0.25);
     return tmp;
   };
   static RangeField getWeight (int i) { return (1.0/6.0); };
