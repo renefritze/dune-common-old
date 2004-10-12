438f8d15aecd00e817959b45f1b89d3468a55f3a
thimo <thimo@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-10-12 13:31:31 +0000

    Finally the DebugStreams do everything I wanted them to do. Please have a look into the extensive Doxygen-docs for details on how to use those beasts
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@845 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/stdstreams.cc
+++ common/stdstreams.cc
@@ -5,20 +5,25 @@
 
 namespace Dune {
 
-/*! \file debugstream.cc */
+/*! \file 
 
-/*! \addtogroup DebugOut 
+The standard debug streams declared in stdstreams.hh exist in this
+file so that they can be compiled into libdune
+
+*/
+
+/*! 
+  \defgroup StdStreams Standard Debug Streams
+  \ingroup DebugOut 
+  @{
 
 Dune defines several standard output streams for the library
 routines.
 
-Note: don't use these streams in applications. You should instead use
-DebugStream to instatiate your own set of independent output streams.
+Applications may control the standard streams via the attach/detach,
+push/pop interface but should define an independent set of streams (see \ref DebugAppl )
 
 */
-
-  
-  /* -- standard streams depending on the debug level -- */
 
   /*! stream for very verbose output: information on the lowest
     level. This is expected to report insane amounts of
@@ -44,5 +49,7 @@ DebugStream to instatiate your own set of independent output streams.
     completely will redirect it. The output of derr is independant on
     the debug-level, only the activation-flag is checked */
   DErrType derr(std::cerr);
+
+  /*! }@ */
 
 };
