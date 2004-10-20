95cfabfaf34519b1195039ac383af88908f68338
thimo <thimo@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-10-20 14:23:55 +0000

    yet another revision: use getrusage(2) which reports spent user time with up to microsecond-resolution (and doesn't need the HZ or CLK_TCK define)
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@949 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/timer.hh
+++ common/timer.hh
@@ -1,54 +1,63 @@
-#ifndef __DUNE_TIMER_HH__
-#define __DUNE_TIMER_HH__
+#ifndef DUNE_TIMER_HH
+#define DUNE_TIMER_HH
 
-//#include<math.h>
-//#include<complex>
-//#include<iostream>
-//#include<iomanip>
-//#include<string>
-#include<sys/times.h>            // for timing measurements
-#include<asm/param.h>
+// headers for getrusage(2)
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+// headers for stderror(3)
+#include <string.h>
+
+// access to errno in C++
+#include <cerrno>
+
+#include "exceptions.hh"
+
 
 /*! \file
     A simple timing class.
 */
 
 namespace Dune {
+
+  class TimerError : public SystemError {} ;
    
-/** @addtogroup ISTL
+/** @addtogroup common
  @{
 */
 
 //! a simple stop watch
-//! using the C command 'times' defined in sys/times.h
+//! using the C command getrusage
 class Timer
 {
 public:
 	//! a new timer, start immediately
-	Timer ()
+        Timer () throw(TimerError)
 	{
-    reset();
+	  reset();
 	}
 
 	//! reset timer
-	void reset ()
+	void reset() throw (TimerError)
 	{
-	  struct tms buf;
-    times(&buf);
-    cstart = buf.tms_utime + buf.tms_stime;
+	  rusage ru;
+	  if (getrusage(RUSAGE_SELF, &ru))
+	    DUNE_THROW(TimerError, strerror(errno));
+	  cstart = ru.ru_utime;
 	}
 
 	//! get elapsed user+sys time in seconds
-	double elapsed ()
-	{
-	  struct tms buf;
-	  times(&buf);
-    clock_t cend = buf.tms_utime + buf.tms_stime;
-    return difftime(cend,cstart)/HZ;
+	double elapsed () const throw (TimerError)
+        {
+	  rusage ru;
+	  if (getrusage(RUSAGE_SELF, &ru))
+	    DUNE_THROW(TimerError, strerror(errno));
+	  return 1.0 * (ru.ru_utime.tv_sec - cstart.tv_sec) + (ru.ru_utime.tv_usec - cstart.tv_usec) / (1000.0 * 1000.0);
 	}
     
-  private:
-  	clock_t cstart;  
+private:
+  struct timeval cstart;
 }; // end class Timer 
 
 /** @} end documentation */
