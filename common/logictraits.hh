c73a31d1da651e56772300ed9e503e69b67d25c4
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-11-05 19:55:25 +0000

    Some template tricks for compile time type information.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1029 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ common/logictraits.hh
@@ -0,0 +1,24 @@
+#ifndef __DUNE_LOCICTRAITS_H__
+#define __DUNE_LOCICTRAITS_H__
+
+namespace Dune{ 
+  /** @addtogroup Common
+   *
+   * @{
+   */
+  template<bool T1, bool T2>
+  struct And
+  {
+    const static bool value=T1 && T2;
+  };
+  
+  template<bool T1, bool T2>
+  struct Or
+  {
+    const static bool value=T1 || T2;
+  };
+  
+  /** @} */
+}
+}
+#endif
