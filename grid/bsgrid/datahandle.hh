82222e874b224bbef7f6d90e83daa1d8e8d46d34
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-11-07 17:31:48 +0000

    In datahandle the interface from BSGrid to Dune for data exchange is implemented.
    Furthermore minor technical changes.
    
    
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1035 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ grid/bsgrid/datahandle.hh
@@ -0,0 +1,43 @@
+#ifndef __DUNE_BSGRID_DATAHANDLE_HH__
+#define __DUNE_BSGRID_DATAHANDLE_HH__
+
+namespace BernhardSchuppGrid {
+
+
+//! the corresponding interface class is defined in bsinclude.hh
+template <class EntityType, class DataCollectorType >
+class GatherScatterImpl : public GatherScatter
+{
+  DataCollectorType & dc_;
+  EntityType & en_;
+
+  typedef ObjectStream ObjectStreamType;
+
+public:
+  GatherScatterImpl(EntityType & en, DataCollectorType & dc) : en_(en), dc_(dc) {}
+
+  //! this method is called from the dunePackAll method of the corresponding 
+  //! Macro element class of the BSGrid, see gitter_dune_pll*.*
+  //! here the data is written to the ObjectStream 
+  virtual void inlineData ( ObjectStreamType & str , HElemType & elem )
+  {
+    // set element and then start 
+    en_.setelement(elem,-1);
+    dc_.inlineData(str,en_);
+  }
+
+  //! this method is called from the duneUnpackSelf method of the corresponding 
+  //! Macro element class of the BSGrid, see gitter_dune_pll*.*
+  //! here the data is read from the ObjectStream 
+  virtual void xtractData ( ObjectStreamType & str , HElemType & elem )
+  {
+    // set element and then start 
+    en_.setelement(elem,-1);
+    dc_.xtractData(str,en_);
+  }
+
+};
+
+}
+
+#endif
