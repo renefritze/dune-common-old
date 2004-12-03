5fa65d4d97324cee43361e669ffef28f98b9e6d0
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-12-03 12:56:12 +0000

    added a method to ask for the grid level
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1206 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/lagrangebase.hh
+++ fem/lagrangebase.hh
@@ -114,6 +114,9 @@ public:
   //! number of unknows for this function space   
   int size () const;
 
+    //! The grid level that this space belongs to
+    int level () const {return level_;}
+
   //! return boundary type for given boundary id 
   //! uses the parameter class BoundaryIdentifierType 
   BoundaryType boundaryType ( int id ) const;
