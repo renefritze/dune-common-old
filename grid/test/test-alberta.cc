9f7b1135270853da22849b0ee3b6532facb093b1
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-12-09 17:35:56 +0000

    all from albert to alberta.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1281 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ grid/test/test-alberta.cc
@@ -0,0 +1,45 @@
+#include <config.h>
+
+/*
+
+  Instantiate Alberta-Grid and feed it to the generic gridcheck()
+
+  Note: Albert needs the defines DIM and DIM_OF_WORLD on the
+  commandline anyway thus we can use them to select the correct class
+
+*/
+
+#include <iostream>
+#include <sstream>
+
+#include <dune/grid/albertagrid.hh>
+
+#include "gridcheck.cc"
+
+int main () {
+  try {
+    /* use grid-file appropriate for dimensions */
+    std::ostringstream filename;
+    filename << "alberta-testgrid-" << DIM << "-" << DIM_OF_WORLD << ".al";
+
+    std::cout << std::endl << "AlbertaGrid<" << DIM << "," << DIM_OF_WORLD 
+	      << "> with grid file: " << filename.str() 
+	      << std::endl << std::endl;
+
+    // extra-environment to check destruction
+    {
+
+      Dune::AlbertaGrid<DIM,DIM_OF_WORLD> grid(filename.str().c_str());
+      gridcheck(grid);
+    };
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+
+  return 0;
+};
