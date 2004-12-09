285ca6d6549a68c32d3ee93d1f0a2e793573589d
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2004-12-09 14:52:35 +0000

    files moved from albertgrid/ to albertagrid/ and albertgrid.cc --> albertagrid.cc and albertextra.hh --> albertaextra.hh
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1250 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ grid/albertagrid/agelementindex.cc
@@ -0,0 +1,37 @@
+#ifndef __ALBERTGRID_ELMEM_CC__
+#define __ALBERTGRID_ELMEM_CC__
+
+namespace AlbertHelp {
+
+// IndexManagerType defined in albertgrid.hh  
+static IndexManagerType * tmpIndexStack = 0;
+
+static void initIndexManager_elmem_cc(IndexManagerType * newIm)
+{
+  tmpIndexStack = newIm;
+  assert(tmpIndexStack != 0);
+}
+
+static void removeIndexManager_elmem_cc()
+{
+  tmpIndexStack = 0;
+}
+
+/* return the new element index for el->index */
+static int get_elIndex()
+{
+  assert(tmpIndexStack != 0);
+  return tmpIndexStack->getIndex();
+}
+
+/* when element is deleted remember the index */
+static void free_elIndex(int ind)
+{
+  assert(tmpIndexStack != 0);
+  tmpIndexStack->freeIndex(ind);
+}
+
+} // end namespace AlbertHelp
+
+#endif
+
