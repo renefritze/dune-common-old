345ccafe6ace08ed214e429c34c9bae5f71428db
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-01-24 12:50:02 +0000

    write/read moved to GrapeDataIO.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1392 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/common/discretefunction.hh
+++ fem/common/discretefunction.hh
@@ -20,38 +20,6 @@ namespace Dune{
   @{
  */
 
-typedef std::basic_string <char> StringType;
-
-    /** \brief ??? 
-     * \todo Please doc me! */
-template <typename T>
-StringType typeIdentifier ()
-{
-  StringType tmp = "unknown";
-  return tmp;
-};
-
-template <>
-StringType typeIdentifier<float> ()
-{
-  StringType tmp = "float";
-  return tmp;
-};
-
-template <>
-StringType typeIdentifier<int> ()
-{
-  StringType tmp = "int";
-  return tmp;
-};
-
-template <>
-StringType typeIdentifier<double> ()
-{
-  StringType tmp = "double";
-  return tmp;
-};
-
 
 //************************************************************************
 //
@@ -228,14 +196,6 @@ public:
   //! add scalar * g to discrete function 
   Vector<RangeFieldType> & 
   add(const Vector<RangeFieldType> &g , RangeFieldType scalar );
-
-  //! write disc func information file and write dofs to file+timestep
-  //! this method use the write method of the implementation of the
-  //! discrete function
-  bool write(const FileFormatType ftype, const char *filename, int timestep, int precision = 6);
-
-  //! same as write only read
-  bool read(const char *filename, int timestep);
 
 private:
   // Barton-Nackman trick 
