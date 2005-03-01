1e74f9bd71e4a1a02880d9aedfb1bd5d36ff4d6f
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-03-01 10:04:13 +0000

    geometryInFather adjusted to the new interface.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1581 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/albertagrid.hh
+++ grid/albertagrid.hh
@@ -136,6 +136,20 @@ public:
   {
     return this->realGeometry.builtGeom(elInfo,face,edge,vertex);
   }
+  
+  // just a wrapper call 
+  void initGeom()
+  {
+    this->realGeometry.initGeom();
+    return ; 
+  }
+
+  // for changing the coordinates of one element 
+  FieldVector<albertCtype, coorddim>& getCoordVec (int i)
+  {
+    return this->realGeometry.getCoordVec(i);
+  }
+
 };
 
 
@@ -225,6 +239,7 @@ public:
   // init geometry with zeros 
   //! no interface method
   void initGeom();
+  FieldVector<albertCtype, cdim>& getCoordVec (int i);
 
   //! print internal data
   //! no interface method
@@ -537,7 +552,7 @@ public:
     We assume that on-the-fly implementation of numerical algorithms 
     is only done for simple discretizations. Assumes that meshes are nested.
   */
-  const Geometry& geometryInFather () const;
+  const Geometry & geometryInFather () const;
   
   /*! Inter-level access to son elements on higher levels<=maxlevel.
     This is provided for sparsely stored nested unstructured meshes.
