2138e209673c6d0219ff68c5996abe73032025fe
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-03-07 19:27:51 +0000

    minor change, compiles again.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1635 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/discfuncarray/dfadapt.cc
+++ fem/discfuncarray/dfadapt.cc
@@ -476,11 +476,12 @@ jacobian (EntityType &en, QuadratureType &quad, int quadPoint, JacobianRangeType
   if(numOfDifferentDofs_ > 1) // i.e. polynom order > 0 
   {
     ret = 0.0;
-    FieldVector<RangeFieldType,dim> tmp(0.0);
+    JacobianRangeType tmp(0.0);
     for(int i=0; i<numOfDifferentDofs_; i++)
     {
-      this->fspace_.getBaseFunctionSet(en).jacobian(i,quad,quadPoint,tmp);
-      inv.umv(tmp,tmpGrad_[0]);  
+      tmpGrad_[0] = 0.0;
+      this->fSpace_.getBaseFunctionSet(en).jacobian(i,quad,quadPoint,tmp);
+      inv.umv(tmp[0],tmpGrad_[0]);  
       
       tmpGrad_[0] *= (* (values_[i]));
 
