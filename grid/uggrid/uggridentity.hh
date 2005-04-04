88fca4f87753b8df4b92a60af5a4297a8e45749b
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-04-04 14:27:29 +0000

    correctly implementing partitionType() for entities of codim==0
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1816 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/uggrid/uggridentity.hh
+++ grid/uggrid/uggridentity.hh
@@ -198,9 +198,23 @@ public:
     int globalIndex() const { return index(); }
 #endif
 
-    /** \brief The partition type for parallel computing
-     * \todo So far it always returns InteriorEntity */
-    PartitionType partitionType () const { return InteriorEntity; }
+    /** \brief The partition type for parallel computing */
+    PartitionType partitionType () const { 
+#ifndef ModelP
+        return InteriorEntity; 
+#else
+#define PARHDRE(p) (&((p)->ge.ddd))
+#define EPRIO(e) DDD_InfoPriority(PARHDRE(e))
+        if (EPRIO(target_) == UG::PrioHGhost
+            || EPRIO(target_) == UG::PrioVGhost
+            || EPRIO(target_) == UG::PrioVHGhost)
+            return GhostEntity;
+        else
+            return InteriorEntity;
+#undef EPRIO
+#undef PARHDRE
+#endif
+    }
 
   //! Geometry of this entity
     const Geometry& geometry () const;
@@ -233,12 +247,12 @@ public:
 
   //! returns true if Entity has children 
     bool isLeaf() const {
+        // return NSONS == 0;
 #ifdef _2
-        int nSons = UG2d::ReadCW(target_, UG2d::NSONS_CE);
+        return UG2d::ReadCW(target_, UG2d::NSONS_CE) == 0;
 #else
-        int nSons = UG3d::ReadCW(target_, UG3d::NSONS_CE);
+        return UG3d::ReadCW(target_, UG3d::NSONS_CE) == 0;
 #endif
-        return nSons==0;
     }
 
   //! Inter-level access to father element on coarser grid. 
