1818d9e71ee7d55a112eddc637a306c299705789
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-04-04 09:43:12 +0000

    Added test for additional functionality.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@1806 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/test/sllisttest.cc
+++ common/test/sllisttest.cc
@@ -48,6 +48,41 @@ void randomizeListFront(Dune::SLList<T,A>& alist){
 	alist.push_back((range*(rand()/(RAND_MAX+1.0))));
 }
 
+int testDeleteNext()
+{
+  typedef Dune::SLList<int,Dune::PoolAllocator<int,8*1024-16> > List;
+  List alist;
+  
+  alist.push_back(3);
+  alist.push_back(4);
+  alist.push_back(5);
+  
+  List::iterator iter=alist.oneBeforeBegin();
+  iter.deleteNext();
+  List::iterator iter1=iter;
+  ++iter1;
+  if(*(alist.begin())!=4){
+    std::cerr<<"delete next on position before head failed!"<<std::endl;
+    return 1;
+  }
+  if(*iter1!=4){
+    std::cerr<<"delete next failed"<<std::endl;
+    return 1;
+  }
+  ++iter;
+  iter.deleteNext();
+  ++iter;
+  if(iter!=alist.end()){
+    std::cerr<<"delete next faild"<<std::endl;
+    return 1;
+  }
+  if(*(alist.tail())!=4){
+    std::cerr<<"delete before tail did not change tail!"<<std::endl;
+  }
+  
+  return 0;
+}
+
 int testInsertAfter()
 {
   typedef Dune::SLList<int,Dune::PoolAllocator<int,8*1024-16> > List;
@@ -75,7 +110,23 @@ int testInsertAfter()
     std::cerr<<"Insertion failed!"<<std::endl;
     ++ret;
   }
+  if(*(alist.begin())!=5){
+    std::cerr<<"Insert after at onebeforeBegin did not change head!"<<std::endl;
+    ++ret;
+  }
+  iter = alist.tail();
+  iter.insertAfter(20);
+  ++iter;
+  if(iter == alist.end() || *iter != 20){
+    std::cerr<<"Insertion failed!"<<std::endl;
+    ++ret;
+  }
 
+  if(*(alist.tail())!=20){
+    std::cerr<<"tail was not changed!!"<<std::endl;
+    ++ret;
+  }
+    
   alist.clear();
   iter=alist.oneBeforeBegin();
   iter.insertAfter(5);
@@ -165,7 +216,7 @@ int main()
   ret+=testPushPop();
   ret+=testOneBeforeBegin(list1);
   ret+=testInsertAfter();
-  
+  ret+=testDeleteNext();
   
   list.clear();
   list1.clear();
