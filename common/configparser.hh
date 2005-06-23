4fe48c11fce9d7be00b5368fd3605d1f45e36749
carsten <carsten@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-06-23 10:26:28 +0000

    class name changed
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2228 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/configparser.hh
+++ common/configparser.hh
@@ -1,48 +1,68 @@
-#ifndef __CONFIGPARSER__HH__
-#define __CONFIGPARSER__HH__
+#ifndef DUNE_CONFIGPARSER_HH
+#define DUNE_CONFIGPARSER_HH
 
 
 #include <map>
 #include <string>
 #include <iostream>
 
-using namespace std;
-
-
-class Config
-{
-	public:
-		
-		Config();
-		
-		void parseFile(string file);
-		void parseCmd(int argc, char* argv []);
-		
-		bool Config::hasKey(const string& key);
-		bool Config::hasSub(const string& key);
-		
-		string& operator[] (const string& key);
-		
-		void report() const;
-		void report(const string prefix) const;
-		
-		Config& sub(const string& key);
-		
-		string get(const string& key, string defaultValue);
-		string get(const string& key, char* defaultValue);
-		int get(const string& key, int defaultValue);
-		double get(const string& key, double defaultValue);
-		bool get(const string& key, bool defaultValue);
+namespace dune {
 	
+	  /** \brief Parser for hierarchical parameter files
+	   * \ingroup Common
+	   *
+	   * This class parses files into a hierachical structure.
+	   * 
+	   */
 	
-	private:
-		map<string, string> values;
-		map<string, Config> subs;
-		string trim(string s);
+	class ConfigParser
+	{
+		public:
+			
+			
+			/** \brief Parser for hierarchical parameter files
+			*
+			* This class parses files into a hierachical structure.
+			* 
+			*/
+			ConfigParser();
+			
+			void parseFile(string file);
+			void parseCmd(int argc, char* argv []);
+			
+			bool hasKey(const string& key);
+			bool hasSub(const string& key);
+			
+			string& operator[] (const string& key);
+			
+			void report() const;
+			void report(const string prefix) const;
+			
+			ConfigParser& sub(const string& key);
+			
+			string get(const string& key, string defaultValue);
+			string get(const string& key, char* defaultValue);
+			int get(const string& key, int defaultValue);
+			double get(const string& key, double defaultValue);
+			
+			/** \brief kurz
+			 *  
+			 * langes
+			 * \param key Der Suchschlüssel
+			 * \param defaultValue
+			 * \return 
+			 */
+			bool get(const string& key, bool defaultValue);
 		
 		
-	
-};
+		private:
+			map<string, string> values;
+			map<string, Config> subs;
+			string trim(string s);
+			
+		
+	};
+} // end namespace dune
 
 
 
