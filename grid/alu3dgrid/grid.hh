6fc095366ee32b352ac0dcdaea3a236f1afb91e5
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-08-10 13:01:14 +0000

    Implemented Global and local is set for ALU3dGrid.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2540 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/alu3dgrid/grid.hh
+++ grid/alu3dgrid/grid.hh
@@ -122,6 +122,13 @@ namespace Dune {
 
     //! Type of the hierarchic index set
     typedef ALU3dGridHierarchicIndexSet<dim,dimworld,elType> HierarchicIndexSetType;
+    
+    //! Type of the global id set 
+    typedef ALU3dGridGlobalIdSet<dim,dimworld,elType> GlobalIdSetType;
+    
+    //! Type of the local id set 
+    typedef ALU3dGridLocalIdSet<dim,dimworld,elType> LocalIdSetType;
+    
     //! Type of the level index set
     typedef DefaultLevelIndexSet<MyType>           LevelIndexSetType; 
     //! Type of the leaf index set
@@ -190,7 +197,7 @@ namespace Dune {
     typename Traits::template Codim<codim>::template Partition<pitype>::LeafIterator
     leafend(int level) const;
 
-   //! General definiton for a leaf iterator
+    //! General definiton for a leaf iterator
     template <int codim>
     typename Traits::template Codim<codim>::LeafIterator
     leafbegin(int level) const;
@@ -237,6 +244,12 @@ namespace Dune {
 
     //! number of grid entities on all levels for given codim
     int global_size (int cd) const ;
+
+    //! get global id set of grid 
+    const GlobalIdSetType & globalIdSet () const { return globalIdSet_; }
+
+    //! get global id set of grid 
+    const LocalIdSetType & localIdSet () const { return localIdSet_; }
 
     //! get hierarchic index set of the grid
     const HierarchicIndexSetType & hierarchicIndexSet () const { return hIndexSet_; }
@@ -357,6 +370,12 @@ namespace Dune {
   
     // our hierarchic index set 
     HierarchicIndexSetType hIndexSet_;
+
+    // out global id set 
+    GlobalIdSetType globalIdSet_; 
+
+    // out global id set 
+    LocalIdSetType localIdSet_;
 
     // the level index set ( default type )
     mutable std::vector < LevelIndexSetType * > levelIndexVec_;
