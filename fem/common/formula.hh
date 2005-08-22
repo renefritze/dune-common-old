eb06f8728c6caa1d07c7b922158be3ac082a9fac
burriad <burriad@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-08-22 12:00:51 +0000

    Needed by boundary
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2579 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ fem/common/formula.hh
@@ -0,0 +1,66 @@
+#ifndef DUNE_FORMULA_HH
+#define DUNE_FORMULA_HH
+
+//- Dune includes
+#include <dune/common/interfaces.hh>
+
+
+namespace Dune {
+  //! \brief Interface class for a generic formula
+  template <class FunctionSpaceImp>
+  class FormulaInterface : public Cloneable {
+  public:
+    //- Typedefs
+    typedef typename FunctionSpaceImp::Domain Domain;
+    typedef typename FunctionSpaceImp::Range Range;
+    typedef typename FunctionSpaceImp::RangeField RangeType;
+    
+    //- Methods
+    virtual void operator() (const Domain& x,
+                             Range& result,
+                             double t = 0.0) const = 0;
+
+  }; // end class FormulaInterface
+
+  //! \brief Constant valued function
+  template <class FunctionSpaceImp>
+  class Constant : public FormulaInterface<FunctionSpaceImp> {
+    typedef FormulaInterface<FunctionSpaceImp> BaseType;
+  public:
+    typedef typename BaseType::Range Range;
+    typedef typename BaseType::Domain Domain;
+
+    //! Constructor with scalar value
+    explicit Constant(typename BaseType::RangeType c = 0.0) : c_(c) {}
+    
+    //! Constructor with vector value
+    explicit Constant(typename BaseType::Range c) : c_(c) {}
+
+    //! Copy constructor
+    Constant(const Constant<FunctionSpaceImp>& other) :
+      c_(other.c_) {}
+
+    //! Assignment operator
+    Constant& operator= (const Constant<FunctionSpaceImp>& other) {
+      if (this != &other) {
+        for (int i = 0; i < Range::size; ++i) {
+          c_[i] = other.c_[i];
+        }
+      }
+      return *this;
+    }
+    
+    virtual void operator() (const Domain& x,
+                             Range& result,
+                             double t = 0.0) const {
+      result = c_;
+    }
+    
+    virtual Constant* clone() const { return new Constant(c_); }
+  private:
+    typename BaseType::Range c_;
+  };
+
+} // end namespace Dune
+
+#endif
