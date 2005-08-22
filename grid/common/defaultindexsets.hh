f42b1191c3158a994c1374c060bd6d2512e9d564
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-08-22 11:20:49 +0000

    Added geomTypes.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2578 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/common/defaultindexsets.hh
+++ grid/common/defaultindexsets.hh
@@ -419,6 +419,12 @@ public:
     size_[cd] = num;
   }
 
+  //! deliver all geometry types used in this grid
+  const std::vector<GeometryType>& geomTypes () const
+  {
+    return grid_.geomTypes();
+  }
+  
 private:
   // grid this level set belongs to 
   const GridType & grid_;
