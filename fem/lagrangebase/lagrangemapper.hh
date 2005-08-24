5df7163532fd441242994160e57f564dfd178c88
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-08-24 14:55:48 +0000

    method indexNew is const now.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2625 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/lagrangebase/lagrangemapper.hh
+++ fem/lagrangebase/lagrangemapper.hh
@@ -79,7 +79,7 @@ public:
   }
 
   //! for dof manager, to check whether it has to copy dof or not 
-  bool indexNew (int num)  
+  bool indexNew (int num) const 
   {
     //int check = (int) (num / dimrange);
     //return indexSet_.indexNew(num,2);
@@ -175,7 +175,7 @@ public:
   }
 
   //! for dof manager, to check whether it has to copy dof or not 
-  bool indexNew (int num)  
+  bool indexNew (int num) const 
   {
     //int check = (int) (num / dimrange);
     //return indexSet_.indexNew(num,2);
@@ -259,7 +259,7 @@ public:
   }
 
   //! for dof manager, to check whether it has to copy dof or not 
-  bool indexNew (int num)  
+  bool indexNew (int num) const
   {
     // all numbers of one entity are maped to the one number of the set 
     const int newn = static_cast<int> (num/dimrange); 
