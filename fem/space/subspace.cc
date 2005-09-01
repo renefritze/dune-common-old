29026ddc9bf9937a8c42e6a6c3ba7a864dd2aa27
burriad <burriad@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-01 15:40:34 +0000

    New subspaces for the combined space
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2715 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ fem/space/subspace.cc
@@ -0,0 +1,4 @@
+
+namespace Dune {
+  
+} // end namespace Dune
---
+++ fem/space/subspace.hh
@@ -0,0 +1,10 @@
+#ifndef DUNE_SUBSPACE_HH
+#define DUNE_SUBSPACE_HH
+
+namespace Dune {
+
+} // end namespace Dune
+
+#include "subspace.cc"
+
+#endif
