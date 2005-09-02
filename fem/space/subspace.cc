e2e9dc33f2fcdda04f4fbda9b53d0679e6dd1568
burriad <burriad@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-02 14:32:38 +0000

    Actual version
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2723 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/space/subspace.cc
+++ fem/space/subspace.cc
@@ -1,4 +1,51 @@
 
 namespace Dune {
+  //- class SubSpace
+  template <class CombinedSpaceImp>
+  const int SubSpace<CombinedSpaceImp>::spaceId_ = 1984;
+
+  template <class CombinedSpaceImp>
+  SubSpace<CombinedSpaceImp>::SubSpace(const CombinedSpaceType& spc,
+                                       int component) :
+      spc_(spc),
+      mapper_(spc, spc.mapper(), component),
+      component_(component)
+    {
+      assert(false);
+      // * more to come here
+    }
+
+  //- class SubBaseFunctionSet
+  template <class CombinedSpaceImp>
+  template <int diffOrd>
+  void SubMapper<CombinedSpaceImp>::
+  evaluate (int baseFunct, 
+            const FieldVector<deriType, diffOrd> &diffVariable, 
+            const DomainType & x, RangeType & phi ) const;
   
+  //! evaluate base function at quadrature point
+  template <int diffOrd, class QuadratureType >
+  void SubMapper<CombinedSpaceImp>::
+  evaluate (int baseFunct, 
+            const FieldVector<deriType, diffOrd> &diffVariable, 
+            QuadratureType & quad, 
+            int quadPoint, RangeType & phi ) const;
+  
+  //- class SubMapper
+  template <class CombinedSpaceImp>
+  int SubMapper<CombinedSpaceImp>::size() const 
+  {
+    assert(false);
+    // *more to come
+  }
+
+  template <class CombinedSpaceImp>
+  template <class EntityType>
+  SubMapper<CombinedSpaceImp>::mapToGlobal(EntityType& en, int localNum) const
+  {
+    assert(false);
+    // *more to come
+  }
+
+
 } // end namespace Dune
