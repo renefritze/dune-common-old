98901a287d8a7ab93e6e4a2e8c4fdbf930579279
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-21 09:20:29 +0000

    changed signature of print.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2918 419231ca-4411-0410-aba2-fc6cf8c4f609

--- quadrature/common/quadrature.hh
+++ quadrature/common/quadrature.hh
@@ -176,7 +176,7 @@ public:
   }
   
   //! pretty print this Quadrature to ostream s
-  void print (std::ostream& s, int indent) const 
+  void print (std::ostream& s) const 
   {
     double sum = 0.0;
     s << "quad (id = " << asImp().getIdentifier() <<") { \n" ;
@@ -202,7 +202,7 @@ template< class RangeFieldType , class DomainType , class QuadratureImp>
 inline std::ostream& operator<< (std::ostream& s, 
     QuadratureDefault<RangeFieldType,DomainType,QuadratureImp>& quad)
 {
-  quad.print(s,0);
+  quad.print(s);
   return s;
 }
 
