84bfedf69ed5a3ead8d84dda7f64f84e11c72b1f
peter <peter@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-28 18:17:24 +0000

    some comments updated
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2966 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/common/intersectioniterator.hh
+++ grid/common/intersectioniterator.hh
@@ -216,7 +216,7 @@ public:
       asImp().increment();
     }
 
-  //! return true if intersection is with boundary. \todo connection with boundary information, processor/outer boundary
+  //! return true if this intersection is with boundary. \todo connection with boundary information, processor/outer boundary
   bool boundary () const
     {
       return asImp().boundary();
@@ -226,7 +226,7 @@ public:
       return asImp().boundaryEntity();
     }
 
-  //! return true if intersection is with neighbor on this level.
+  //! return true if intersection is with another element (might be on a lower level).
   bool neighbor () const
     {
       return asImp().neighbor();
