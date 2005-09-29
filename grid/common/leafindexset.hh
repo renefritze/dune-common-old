303757c3f50b24446385ce9a2308945d04e810a4
mario <mario@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-29 08:13:27 +0000

    small typos in comment
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@2979 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/common/leafindexset.hh
+++ grid/common/leafindexset.hh
@@ -321,7 +321,7 @@ public:
   
   void print (const char * msg, bool oldtoo = false ) const;
 
-}; // end of class AdaptiveLeafIndexSet 
+}; // end of class 
 
 //******************************************************************
 //
@@ -330,9 +330,9 @@ public:
 //
 //******************************************************************
 /*! 
-  This index set generates and consecutive leaf index out of the unique
+  This index set generates a consecutive leaf index out of the unique
   global index of each entity. This index set can be used instead of the
-  default grid index sets an can be generated for each grid implementation.
+  default grid index sets and can be generated for each grid implementation.
 
   Note that only codim = 0 is working at the moment. 
 
