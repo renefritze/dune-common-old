58bced659e7f82f235b46572395d78e76548a8a9
peter <peter@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-09-30 18:19:30 +0000

    - correct determination if an element is copied to next finer level that works also in parallel version. Do not use nSons==1 in that case.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3000 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/uggrid/uggridindexsets.hh
+++ grid/uggrid/uggridindexsets.hh
@@ -572,7 +572,7 @@ public:
 						typename TargetType<0,dim>::T* father_ = UG_NS<dim>::EFather(target_);
 						while (father_!=0)
 						  {
-							if (UG_NS<dim>::nSons(father_)!=1) break; // handle only copies
+							if (!UG_NS<dim>::hasCopy(father_)) break; // handle only copies
 							UG_NS<dim>::leafIndex(UG_NS<dim>::GetEdge(UG_NS<dim>::Corner(father_,renumberVertex(gt,a)),UG_NS<dim>::Corner(father_,renumberVertex(gt,b)))) = index;
 							father_ = UG_NS<dim>::EFather(father_);
 						  }
@@ -603,7 +603,7 @@ public:
 						  typename TargetType<0,dim>::T* father_ = UG_NS<dim>::EFather(target_);
 						  while (father_!=0)
 							{
-							  if (UG_NS<dim>::nSons(father_)!=1) break; // handle only copies
+							  if (!UG_NS<dim>::hasCopy(father_)) break; // handle only copies
 							  UG_NS<dim>::leafIndex(UG_NS<dim>::SideVector(father_,renumberFace(gt,i))) = index;
 							  father_ = UG_NS<dim>::EFather(father_);
 							}
