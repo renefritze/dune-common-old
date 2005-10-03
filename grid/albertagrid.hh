2d984c0c1673109b1e1bf3ceb7f4126a89d44668
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-10-03 10:45:08 +0000

    minor changes on comments.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3006 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/albertagrid.hh
+++ grid/albertagrid.hh
@@ -1464,23 +1464,23 @@ namespace Dune
      */
     int size (int level, int codim) const; 
 
-	//! number of leaf entities per codim in this process
-	int size (int codim) const
-	{
-	  return size(codim,simplex);
-	}
+    //! number of leaf entities per codim in this process
+    int size (int codim) const
+    {
+      return size(codim,simplex);
+    }
 
-	//! number of entities per level, codim and geometry type in this process
-	int size (int level, int codim, GeometryType type) const
-	{
-	  return this->levelIndexSet(level).size(codim,type);
-	}
+    //! number of entities per level, codim and geometry type in this process
+    int size (int level, int codim, GeometryType type) const
+    {
+      return this->levelIndexSet(level).size(codim,type);
+    }
 
-	//! number of leaf entities per codim and geometry type in this process
-	int size (int codim, GeometryType type) const
-	{
-	  return this->leafIndexSet().size(codim,type);
-	}
+    //! number of leaf entities per codim and geometry type in this process
+    int size (int codim, GeometryType type) const
+    {
+      return this->leafIndexSet().size(codim,type);
+    }
 
     /** \brief ghostSize is zero for this grid  */
     int ghostSize (int level, int codim) const { return 0; } 
@@ -1494,7 +1494,7 @@ namespace Dune
     /*! \brief marks an element for refCount refines. if refCount is negative the
      *  element is coarsend -refCount times
      *  NOTE: if element was already marked for refinement then nothing
-     *  happens if element is again marked for coarsen, refinement alsway
+     *  happens if element is again marked for coarsen, refinement always
      *  counts more then coarsening 
      *  mark returns true if element was marked, otherwise false */
     bool mark( int refCount , typename Traits::template Codim<0>::EntityPointer & en ) const; 
