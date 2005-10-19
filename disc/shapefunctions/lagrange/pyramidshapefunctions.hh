deb39f37e363a6883583be01b76f322e9ae34fba
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-10-19 18:44:21 +0000

    As prism and pyramid only exist for dim=3 it makes no sense to provide generic shape functions for all dimensions (that do nonesense) together with a specialization for dim=3.
    Moved the specialization to
    LagrangeShapeFunctionSetContainer. Pyramid and Prism are only
    available in the specialization for dim=3. For all other dimension an
    exception is thrown if they are requested.
    
    Still problems compiling dune-dd with g++-3.4.1:
    
    /afs/iwr.uni-heidelberg.de/home/mblatt/dune/dune/dune/disc/shapefunctions/lagrange/pyramidshapefunctions.hh: In member function `T Dune::P1PyramidShapeFunction<C, T>::evaluateFunction(int, const Dune::FieldVector<C, 3>&) const [with C = Dune::sgrid_ctype, T = testadaptivity::NumberType]':
    /afs/iwr.uni-heidelberg.de/home/mblatt/dune/dune/dune/disc/shapefunctions/lagrange/pyramidshapefunctions.hh:303: interner Compiler-Fehler: in move_for_stack_reg, bei reg-stack.c:1065
    Please submit a full bug report,
    with preprocessed source if appropriate.
    See <URL:http://gcc.gnu.org/bugs.html> for instructions.
    For Debian GNU/Linux specific bug reporting instructions,
    see <URL:file:///usr/share/doc/gcc-3.4/README.Bugs>
    
    Will investigate further: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=17186
    
    
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3270 419231ca-4411-0410-aba2-fc6cf8c4f609

--- disc/shapefunctions/lagrange/pyramidshapefunctions.hh
+++ disc/shapefunctions/lagrange/pyramidshapefunctions.hh
@@ -35,77 +35,8 @@ namespace Dune
    * A class for piecewise constant shape functions in a pyramid
    * Evaluation is done at the centroid(Centre of gravity) of the pyramid
    */
-
-
-  template<typename C, typename T, int d>
-  class P0PyramidShapeFunction
-  {
-  public:
-
-	// compile time sizes
-	enum { dim=d };    // maps from R^d
-	enum { comps=1 };      // to R^1
-
-	enum { m=1 }; // total number of basis functions
-
-	// export types
-	typedef C CoordType;
-	typedef T ResultType;
-	typedef P0PyramidShapeFunction ImplementationType;
-
-	//! make a shape function object
-	P0PyramidShapeFunction ()
-	{}
-
-	//! evaluate shape function in local coordinates
-	ResultType evaluateFunction (int comp, const FieldVector<CoordType,d>& x) const
-	{
-	  return 1;
-	}
-
-	//! evaluate gradient in local coordinates
-	ResultType evaluateDerivative (int comp, int dir, const FieldVector<CoordType,d>& x) const
-	{
-	  return 0;
-	}
-
-	//! consecutive number of associated dof within element
-	int localindex (int comp) const
-	{
-	  return 0;
-	}
-
-	//! codim of associated dof
-	int codim () const
-	{
-	  return 0;
-	}
-
-	//! entity (of codim) of associated dof
-	int entity () const
-	{
-	  return 0;
-	}
-
-	//! consecutive number of dof within entity
-	int entityindex () const
-	{
-	  return 0;
-	}
-
-	//! interpolation point associated with shape function
-	const FieldVector<CoordType,dim>& position () const
-	{
-	  return pos;
-	}
-	
-  private:
-	FieldVector<CoordType,d> pos;
-  };
-
-
   template<typename C, typename T>
-  class P0PyramidShapeFunction<C,T,3>
+  class P0PyramidShapeFunction
   {
   public:
 
@@ -177,13 +108,13 @@ namespace Dune
   };
 
 
-  template<typename C, typename T, int d, typename S>
+  template<typename C, typename T, typename S>
   class P0PyramidShapeFunctionSet
   {
   public:
 
 	// compile time sizes
-	enum { dim=d };    // maps from R^d
+	enum { dim=3 };    // maps from R^d
 	enum { comps=1 };      // to R^1
 
 	enum { m=1 }; // total number of basis functions
@@ -243,82 +174,8 @@ namespace Dune
    * A class for piecewise constant shape functions in a pyramid
    * Evaluation is done at the quadrature points the pyramid
    */
-
-
-  template<typename C, typename T, int d>
-  class P1PyramidShapeFunction
-  {
-  public:
-
-	// compile time sizes
-	enum { dim=d };    // maps from R^d
-	enum { comps=1 };      // to R^1
-
-	enum { m=1 }; // total number of basis functions
-
-	// export types
-	typedef C CoordType;
-	typedef T ResultType;
-	typedef P1PyramidShapeFunction ImplementationType;
-
-	//! make a shape function object
-	P1PyramidShapeFunction (int i)
-	{}
-
-	//! must be defaultconstructible
-	P1PyramidShapeFunction ()
-    {}
-
-	//! evaluate shape function in local coordinates
-	ResultType evaluateFunction (int comp, const FieldVector<CoordType,d>& x) const
-	{
-	  return 1;
-	}
-
-	//! evaluate gradient in local coordinates
-	ResultType evaluateDerivative (int comp, int dir, const FieldVector<CoordType,d>& x) const
-	{
-	  return 0;
-	}
-
-	//! consecutive number of associated dof within element
-	int localindex (int comp) const
-	{
-	  return 0;
-	}
-
-	//! codim of associated dof
-	int codim () const
-	{
-	  return 0;
-	}
-
-	//! entity (of codim) of associated dof
-	int entity () const
-	{
-	  return 0;
-	}
-
-	//! consecutive number of dof within entity
-	int entityindex () const
-	{
-	  return 0;
-	}
-
-	//! interpolation point associated with shape function
-	const FieldVector<CoordType,dim>& position () const
-	{
-	  return pos;
-	}
-	
-  private:
-	FieldVector<CoordType,d> pos;
-  };
-
-
-
   template<typename C, typename T>
-  class P1PyramidShapeFunction<C,T,3>
+  class P1PyramidShapeFunction
   {
   public:
 
@@ -443,6 +300,8 @@ namespace Dune
 	      std::cout<<"wrong no of shape fns in Pyramid?"<<'\n';
 	      break;
 	    }
+	  0;
+	  
 	}
 
     //! evaluate gradient in local coordinates
@@ -678,13 +537,13 @@ namespace Dune
   };
 
 
-  template<typename C, typename T, int d, typename S>
+  template<typename C, typename T, typename S>
   class P1PyramidShapeFunctionSet
   {
   public:
 
 	// compile time sizes
-	enum { dim=d };    // maps from R^d
+	enum { dim=3 };    // maps from R^d
 	enum { comps=1 };      // to R^1
 
 	enum { m=5 }; // total number of basis functions
@@ -740,10 +599,8 @@ namespace Dune
 
 
   //! P0 shape functions in the pyramid without virtual functions
-  template<typename C, typename T, int d>
-  class P0PyramidShapeFunctionSetContainer;
-  template<typename C, typename T>
-  class P0PyramidShapeFunctionSetContainer<C,T,3>
+    template<typename C, typename T>
+  class P0PyramidShapeFunctionSetContainer
   {
   public:
 	// compile time sizes
@@ -754,7 +611,7 @@ namespace Dune
 	// exported types
 	typedef C CoordType; 
 	typedef T ResultType;
-	typedef P0PyramidShapeFunctionSet<C,T,3,P0PyramidShapeFunction<C,T,3> > value_type;
+	typedef P0PyramidShapeFunctionSet<C,T,P0PyramidShapeFunction<C,T> > value_type;
 
 	const value_type& operator() (GeometryType type, int order) const
 	{
@@ -767,10 +624,9 @@ namespace Dune
   };
  
   //! P1 shape functions in the pyramid without virtual functions
-  template<typename C, typename T, int d>
-  class P1PyramidShapeFunctionSetContainer;
+
   template<typename C, typename T>
-  class P1PyramidShapeFunctionSetContainer<C,T,3>
+  class P1PyramidShapeFunctionSetContainer
   {
   public:
 	// compile time sizes
@@ -781,7 +637,7 @@ namespace Dune
 	// exported types
 	typedef C CoordType; 
 	typedef T ResultType;
-	typedef P1PyramidShapeFunctionSet<C,T,3,P1PyramidShapeFunction<C,T,3> > value_type;
+	typedef P1PyramidShapeFunctionSet<C,T,P1PyramidShapeFunction<C,T> > value_type;
 
 	const value_type& operator() (GeometryType type, int order) const
 	{
