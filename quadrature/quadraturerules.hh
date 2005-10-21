f8ed7d6b863600a8b464aaa95fc53b21cfb4df95
sreejith <sreejith@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-10-21 21:25:24 +0000

    missing cases added in the constructor. Also couldnt find symmetric quad- rules for order > 3 for tetrahedron from ugquadrature file
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3281 419231ca-4411-0410-aba2-fc6cf8c4f609

--- quadrature/quadraturerules.hh
+++ quadrature/quadraturerules.hh
@@ -1092,7 +1092,28 @@ public:
       case 5:
 	m=7;
 	break;
-      default: m=7;
+      case 6:
+	m=12;
+	break;
+      case 7:
+	m=12;
+	break;
+      case 8:
+	m=16;
+	break;
+      case 9:
+	m=19;
+	break;
+      case 10:
+	m=25;
+	break;
+      case 11:
+	m=28;
+	break;
+      case 12:
+	m=33;
+	break;
+      default: m=33;
       }
   delivered_order = SimplexQuadraturePointsSingleton<2>::sqp.order(m);
   FieldVector<ct, d> local;
