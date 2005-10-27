525760ff1f18e12ca9d4af02b4c48e0e5fadbbcc
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-10-27 14:46:20 +0000

    bug fix, now Grape can be called more than once. :)
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3321 419231ca-4411-0410-aba2-fc6cf8c4f609

--- io/visual/grape/ghmesh.cc
+++ io/visual/grape/ghmesh.cc
@@ -870,6 +870,21 @@ inline void * hmesh(int (* const f_leaf)(DUNE_ELEM *), int (* const n_leaf)(DUNE
   return ((void *) mesh);
 }
 
+static inline void addProjectUIF() 
+{
+  static int firstCall = 1;
+ 
+  // only call this once otherwise 
+  // grape cannot be runed twice with the same program 
+  if(firstCall) 
+  {
+    char p_name[32];
+    sprintf(p_name,"uif-m%d",GRAPE_DIM);
+    g_project_add(p_name);
+    firstCall = 0;
+  }
+}
+
 /* forward declaration */
 static void grape_add_remove_methods(void);
 
@@ -881,10 +896,8 @@ inline void handleMesh(void *hmesh)
     MANAGER * mgr = (MANAGER *)GRAPE(Manager,"get-stdmgr")();
     SCENE  *sc = (SCENE *)GRAPE(Scene,"new-instance")("dune hmesh");
 
-    char p_name[32];
-    sprintf(p_name,"uif-m%d",GRAPE_DIM);
-    g_project_add(p_name);
-
+    addProjectUIF();
+    
     if(!mesh->f_data)
     {
       /* if no data then switch to grid mode */
