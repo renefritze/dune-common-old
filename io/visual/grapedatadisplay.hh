033e240310d6d924c5e207c5e51c42a2e9f4ad4e
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-12-01 11:43:23 +0000

    Modified template parameter list od GrapeDataDisplay. Now the DiscreteFunctionType is determined when addData is called. This enables visualisation of different DiscreteFunctionType (i.e. different polynomial order).
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3543 419231ca-4411-0410-aba2-fc6cf8c4f609

--- io/visual/grapedatadisplay.hh
+++ io/visual/grapedatadisplay.hh
@@ -21,13 +21,54 @@ class GrapeLagrangePoints;
   GrapeDataDisplay   
 */
 
-template<class GridType, class DiscFuncType>
+template <class GridType, class DiscreteFunctionType> 
+struct EvalDiscreteFunctions 
+{
+  typedef typename GridType :: template Codim<0> :: Entity EntityType;
+  enum { dim = GridType::dimension };
+  enum { dimworld = GridType::dimensionworld };
+
+  typedef typename GridType :: ctype ctype;
+
+  typedef typename DiscreteFunctionType :: LocalFunctionType LocalFunctionType; 
+  typedef typename DiscreteFunctionType :: FunctionSpaceType FunctionSpaceType;
+
+  typedef typename FunctionSpaceType :: RangeType RangeType;
+  typedef typename FunctionSpaceType :: DomainType DomainType;
+
+  typedef typename GrapeInterface<dim,dimworld>::DUNE_ELEM  DUNE_ELEM;
+  typedef typename GrapeInterface<dim,dimworld>::DUNE_FDATA DUNE_FDATA;
+
+  // for the data visualization
+  inline static void evalCoordNow (EntityType &en, DUNE_FDATA *fdata, const double *coord, double * val);
+
+  // for the data visualization
+  inline static void evalDofNow (EntityType &en, int geomType, DUNE_FDATA *fdata , int localNum, double * val);
+
+  inline static void evalCoord (DUNE_ELEM *he, DUNE_FDATA *df, 
+                  const double *coord, double * val);
+  
+  inline static void evalDof (DUNE_ELEM *he, DUNE_FDATA *df, int localNum, double * val);
+  
+  // for the data visualization
+  inline static void evalScalar (EntityType &en, int geomType, 
+      DiscreteFunctionType & func, LocalFunctionType &lf, 
+                const int * comp , int localNum, double * val);
+  
+  // for the data visualization
+  inline static void evalVector (EntityType &en, int geomType, 
+      DiscreteFunctionType & func, LocalFunctionType &lf, 
+                const int * comp, int vend, int localNum, double * val);
+};
+
+    
+template<class GridType>
 class GrapeDataDisplay : public GrapeGridDisplay < GridType >
 {
-  typedef GrapeDataDisplay < GridType , DiscFuncType > MyDisplayType;
-  typedef typename DiscFuncType::FunctionSpaceType FunctionSpaceType;
-  typedef typename FunctionSpaceType::DomainType DomainType;
-  typedef typename FunctionSpaceType::RangeType RangeType;
+  typedef GrapeDataDisplay < GridType > MyDisplayType;
+  //typedef typename DiscFuncType::FunctionSpaceType FunctionSpaceType;
+  //typedef typename FunctionSpaceType::DomainType DomainType;
+  //typedef typename FunctionSpaceType::RangeType RangeType;
     
   enum { dim = GridType::dimension };
   enum { dimworld = GridType::dimensionworld };
@@ -40,9 +81,9 @@ class GrapeDataDisplay : public GrapeGridDisplay < GridType >
 public:
 
   typedef typename GridType::Traits::template Codim<0>::LevelIterator LevelIterator;
-  typedef DiscFuncType DiscreteFunctionType;
-
-  enum { polynomialOrder = FunctionSpaceType::polynomialOrder };
+  //typedef DiscFuncType DiscreteFunctionType;
+  //enum { polynomialOrder = FunctionSpaceType::polynomialOrder };
+  enum { polynomialOrder = 1 };
 
 public:
   //! Constructor, make a GrapeDataDisplay for given grid and myRank = -1
@@ -55,6 +96,7 @@ public:
 
   //! Calls the display of the grid and draws the discrete function
   //! if discretefunction is NULL, then only the grid is displayed 
+  template <class DiscFuncType>
   inline void dataDisplay(DiscFuncType &func, bool vector = false);
 
   //! Calls the display of the grid and draws the discrete function
@@ -63,9 +105,11 @@ public:
   inline void displayVector(const VectorPointerType * vector);
 
   //! add discrete function to display  
+  template <class DiscFuncType>
   inline void addData(DiscFuncType &func, const DATAINFO * , double time );
   
   //! add discrete function to display  
+  template <class DiscFuncType>
   inline void addData(DiscFuncType &func, const char * name , double time , bool vector = false );
 
   // retrun whether we have data or not 
@@ -82,31 +126,11 @@ private:
   // store lagrange points for evaluation 
   GrapeLagrangePoints<ctype,dim,dimworld,polynomialOrder> lagrangePoints_;
 
-  // tmp variables 
-  RangeType  tmp_;
-  DomainType domTmp_;
+  typedef typename GridType :: template Codim<0> :: Entity EntityCodim0Type;
+  typedef void evalCoord_t(EntityCodim0Type &, DUNE_FDATA *, const double *, double * );
+  typedef void evalDof_t  (EntityCodim0Type &,int , DUNE_FDATA * , int , double * );
 
-  // for the data visualization
-  template <class EntityType>
-  inline void evalCoord (EntityType &en, DUNE_FDATA *, const double *coord, double * val);
-
-  // for the data visualization
-  template <class EntityType>
-  inline void evalDof (EntityType &en, int geomType, DUNE_FDATA * , int localNum, double * val);
-  // for the data visualization
-  template <class EntityType, class LocalFuncType>
-  inline void evalScalar (EntityType &en, int geomType, DiscFuncType & func, LocalFuncType &lf, 
-                const int * comp , int localNum, double * val);
-  
-  // for the data visualization
-  template <class EntityType, class LocalFuncType>
-  inline void evalVector (EntityType &en, int geomType, DiscFuncType & func,LocalFuncType &lf, 
-                const int * comp, int vend, int localNum, double * val);
 public:  
-  inline void evalCoord (DUNE_ELEM *he, DUNE_FDATA *df, 
-                  const double *coord, double * val);
-  inline void evalDof (DUNE_ELEM *he, DUNE_FDATA *df, int localNum, double * val);
-
 protected:
   
   inline static void func_real (DUNE_ELEM *he , DUNE_FDATA * fe,int ind, 
@@ -171,6 +195,7 @@ private:
   }
 };
 
+/*
 template <int polOrd>
 struct GrapeVectorDisplay 
 {
@@ -192,6 +217,7 @@ struct GrapeVectorDisplay
     disp.displayVector( vector );
   }
 };
+*/
   
 } // end namespace Dune
 
