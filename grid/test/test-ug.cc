bc806e413578dc4675d6ba4e6420cb637973875e
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-12-08 15:20:12 +0000

    enabled check for hybrid grids.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3564 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/test/test-ug.cc
+++ grid/test/test-ug.cc
@@ -17,27 +17,62 @@
 
 #include "gridcheck.cc"
 
+template <class GridType >
+void markOne ( GridType & grid , int num , int ref )
+{
+  typedef typename GridType::template Codim<0>::LeafIterator LeafIterator;
+
+  int count = 0;
+  
+  LeafIterator endit = grid.template leafend  <0> ();
+  for(LeafIterator it = grid.template leafbegin<0> (); it != endit ; ++it )
+  {
+    if(num == count) grid.mark( ref, it );
+    count++;
+  }
+
+  grid.preAdapt();
+  grid.adapt();
+  grid.postAdapt();
+}
+
 int main () {
   try {
 
     // extra-environment to check destruction
     {
-        std::cout << std::endl << "UGGrid<2,2> with grid file: ug-testgrid-2.am"
-	      << std::endl << std::endl;
-        Dune::UGGrid<2,2> grid;
-        Dune::AmiraMeshReader<Dune::UGGrid<2,2> >::read(grid, "ug-testgrid-2.am");
+      std::cout << std::endl << "UGGrid<2,2> with grid file: ug-testgrid-2.am"
+	    << std::endl << std::endl;
+      Dune::UGGrid<2,2> grid;
+      Dune::AmiraMeshReader<Dune::UGGrid<2,2> >::read(grid, "ug-testgrid-2.am");
 
+      // check macro grid 
       gridcheck(grid);
-    };
+
+      // create hybrid grid 
+      markOne(grid,0,1) ;
+      gridcheck(grid);
+
+      grid.globalRefine(1);
+      gridcheck(grid);
+    }
 
     {
-        std::cout << std::endl << "UGGrid<3,3> with grid file: ug-testgrid-3.am"
-	      << std::endl << std::endl;
-        Dune::UGGrid<3,3> grid;
-        Dune::AmiraMeshReader<Dune::UGGrid<3,3> >::read(grid, "ug-testgrid-3.am");
+      std::cout << std::endl << "UGGrid<3,3> with grid file: ug-testgrid-3.am"
+	    << std::endl << std::endl;
+      Dune::UGGrid<3,3> grid;
+      Dune::AmiraMeshReader<Dune::UGGrid<3,3> >::read(grid, "ug-testgrid-3.am");
 
+      // check macro grid 
       gridcheck(grid);
-    };
+
+      // create hybrid grid 
+      markOne(grid,0,1) ;
+      gridcheck(grid);
+
+      grid.globalRefine(1);
+      gridcheck(grid);
+    }
 
   } catch (Dune::Exception &e) {
     std::cerr << e << std::endl;
