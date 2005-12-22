0ade86bef75b4e25f68195b1fe21443d1b14eb0b
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2005-12-22 13:52:20 +0000

    a test for the 2d Alberta writer
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3736 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ io/file/test/test-albertawriter.cc
@@ -0,0 +1,31 @@
+#include "config.h"
+
+#include <dune/grid/albertagrid.hh>
+#include <dune/io/file/albertawriter.hh>
+
+using namespace Dune;
+
+int main()
+{
+
+    // ////////////////////////////////////////////
+    //   Load a 2d test grid
+    // ////////////////////////////////////////////
+    AlbertaGrid<2,2> albertaGrid("alberta-testgrid-2-2.al");
+
+    // ////////////////////////////////////////////
+    //   Save it to a temporary file
+    // ////////////////////////////////////////////
+    AlbertaWriter<AlbertaGrid<2,2> >::writeGrid(albertaGrid, "temporaryGrid.al");
+
+    // ////////////////////////////////////////////
+    //   Check whether written grid is readable
+    // ////////////////////////////////////////////
+    AlbertaGrid<2,2> testGrid("temporaryGrid.al");
+
+    // ////////////////////////////////////////////
+    //   Remove temporary grid
+    // ////////////////////////////////////////////
+    system("rm temporaryGrid.al");
+
+}
