476f04c7527f502bd56316e18cddb16408fd8add
oliver <oliver@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-01-23 08:55:50 +0000

    wrap UG method GetNodeContext
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@3925 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/uggrid/ugfunctions.hh
+++ grid/uggrid/ugfunctions.hh
@@ -196,6 +196,12 @@ public:
         return UG2d::GetSons(element, sonList);
     }
 
+    /** \todo Remove the const casts */
+    static int GetNodeContext(const UG2d::element* element, const UG2d::node** context) {
+        return UG2d::GetNodeContext(const_cast<UG2d::element*>(element), 
+                                    const_cast<UG2d::node**>(context));
+    }
+
     //! Encapsulates the GRID_ATTR macro
     static int Grid_Attr(const UGTypes<2>::GridType* grid) {
         return GRID_ATTR(grid);
