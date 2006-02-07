f89e00e3d78bdad6509cce41483c7773c95441a8
peter <peter@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-02-07 20:28:36 +0000

    Bug fix: element type is written as unsigned char and not as int! Now binaryappended mode works again. Grrrrr!
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4033 419231ca-4411-0410-aba2-fc6cf8c4f609

--- io/file/vtk/vtkwriter.hh
+++ io/file/vtk/vtkwriter.hh
@@ -229,7 +229,7 @@ namespace Dune
 			{
 			  sprintf(fullname,"%s-%04d.pvtu",name,grid.comm().size());
 			  file.open(fullname);
-			  writeParallelHeader(file,name);
+			  writeParallelHeader(file,name,"");
 			  file.close();
 			}
 		  grid.comm().barrier();
@@ -237,7 +237,7 @@ namespace Dune
 	}
 
 	//! write output; interface might change later
-	void pwrite (const char* name, const char* piecename, VTKOptions::Type dm = VTKOptions::ascii)
+	void pwrite (const char* name,  const char* path, VTKOptions::Type dm = VTKOptions::ascii)
 	{
 	  // make data mode visible to private functions
 	  datamode=dm;
@@ -249,7 +249,7 @@ namespace Dune
 		{
 		  std::ofstream file;
 		  char fullname[128];
-		  sprintf(fullname,"%s.vtu",name);
+		  sprintf(fullname,"%s%s.vtu",path,name);
 		  if (datamode==VTKOptions::binaryappended)
 			file.open(fullname,std::ios::binary);
 		  else
@@ -261,7 +261,7 @@ namespace Dune
 		{
 		  std::ofstream file;
 		  char fullname[128];
-		  sprintf(fullname,"%s-%04d-%04d.vtu",piecename,grid.comm().size(),grid.comm().rank());
+		  sprintf(fullname,"%s%s-%04d-%04d.vtu",path,name,grid.comm().size(),grid.comm().rank());
 		  if (datamode==VTKOptions::binaryappended)
 			file.open(fullname,std::ios::binary);
 		  else
@@ -271,9 +271,9 @@ namespace Dune
 		  grid.comm().barrier();
 		  if (grid.comm().rank()==0)
 			{
-			  sprintf(fullname,"%s-%04d.pvtu",name,grid.comm().size());
+			  sprintf(fullname,"%s%s-%04d.pvtu",path,name,grid.comm().size());
 			  file.open(fullname);
-			  writeParallelHeader(file,piecename);
+			  writeParallelHeader(file,name,"");
 			  file.close();
 			}
 		  grid.comm().barrier();
@@ -315,7 +315,7 @@ namespace Dune
       }
 
 	//! write header file in parallel case to stream
-	void writeParallelHeader (std::ostream& s, const char* piecename)
+	void writeParallelHeader (std::ostream& s, const char* piecename, const char* piecepath)
 	{
 	  // xml header
 	  s << "<?xml version=\"1.0\"?>" << std::endl;
@@ -407,7 +407,7 @@ namespace Dune
 	  for (int i=0; i<grid.comm().size(); i++)
 		{
 		  char fullname[128];
-		  sprintf(fullname,"%s-%04d-%04d.vtu",piecename,grid.comm().size(),i);
+		  sprintf(fullname,"%s%s-%04d-%04d.vtu",piecepath,piecename,grid.comm().size(),i);
 		  indent(s); s << "<Piece Source=\"" << fullname << "\"/>" << std::endl;
 		}
 
@@ -417,8 +417,7 @@ namespace Dune
 
 	  // /VTKFile
 	  indentDown();
-	  s << "</VTKFile>" << std::endl;
-	  
+	  s << "</VTKFile>" << std::endl;	  
 	}
 
 	//! write data file to stream
@@ -761,7 +760,7 @@ namespace Dune
 	  for (CellIterator it=is.template begin<0,vtkPartition>(); it!=is.template end<0,vtkPartition>(); ++it)
 		if (it->partitionType()==InteriorEntity)
 		  {
-            int vtktype = vtkType(it->geometry().type());
+            unsigned char vtktype = vtkType(it->geometry().type());
 			stream.write(vtktype);
 		  }
 
