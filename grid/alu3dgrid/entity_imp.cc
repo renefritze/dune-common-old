773a20d054436bacf6259ed6fc09ea883a745f0d
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-03-03 13:36:47 +0000

    minor change.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4243 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/alu3dgrid/entity_imp.cc
+++ grid/alu3dgrid/entity_imp.cc
@@ -710,7 +710,7 @@ namespace Dune {
   inline ALU3dGridEntityPointerBase<codim,GridImp> :: 
   ~ALU3dGridEntityPointerBase()
   {
-    if(entity_) grid_.freeEntity( entity_ );
+    this->done();
   }
 
   template<int codim, class GridImp >
