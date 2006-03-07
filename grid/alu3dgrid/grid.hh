3a49fb35c4926790e8220f9cfcff479580f3a0d6
peter <peter@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-03-07 23:50:07 +0000

    made communicate const Robert: hope this does not break your code
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4293 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/alu3dgrid/grid.hh
+++ grid/alu3dgrid/grid.hh
@@ -452,7 +452,7 @@ namespace Dune {
 
     /** dummy communicate */
     template<class DataHandle>
-    void communicate (DataHandle& data, InterfaceType iftype, CommunicationDirection dir);
+    void communicate (DataHandle& data, InterfaceType iftype, CommunicationDirection dir) const;
 
     /** dummy collective communication */
     const CollectiveCommunication<ALU3dGrid>& comm () const
