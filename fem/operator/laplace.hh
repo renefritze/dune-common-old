e16286e342b1e7a0231e4d85f5811e0434f96fe1
haasdonk <haasdonk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-03-16 18:04:37 +0000

    removed index out of range bug...
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4318 419231ca-4411-0410-aba2-fc6cf8c4f609

--- fem/operator/laplace.hh
+++ fem/operator/laplace.hh
@@ -193,7 +193,7 @@ namespace Dune
             }
             
             for(i=0; i<matSize; i++) 
-                for (j=matSize; j>i; j--) 
+                for (j=matSize-1; j>i; j--) 
                     mat[j][i] = mat[i][j];
             
         }
