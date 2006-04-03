7728e703b124c095594c11b8ea1c44e0f97e0b02
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-04-03 19:32:24 +0000

    removed last file, and adapted parsetest.cc
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4447 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/test/parsetest.cc
+++ common/test/parsetest.cc
@@ -20,13 +20,10 @@ find dune/common/ -maxdepth 1 -name \*.hh -exec echo '#include <{}>' \; | sort
 #include <dune/common/configparser.hh>
 #include <dune/common/debugstream.hh>
 #include <dune/common/dlist.hh>
-#include <dune/common/dynamictype.hh>
 #include <dune/common/enumset.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/fixedarray.hh>
 #include <dune/common/fmatrix.hh>
-#include <dune/common/function.hh>
-#include <dune/common/functionspace.hh>
 #include <dune/common/fvector.hh>
 #include <dune/common/gcd.hh>
 #include <dune/common/genericiterator.hh>
@@ -35,10 +32,8 @@ find dune/common/ -maxdepth 1 -name \*.hh -exec echo '#include <{}>' \; | sort
 #include <dune/common/iteratorfacades.hh>
 #include <dune/common/lcm.hh>
 #include <dune/common/logictraits.hh>
-#include <dune/common/mapping.hh>
 #include <dune/common/matrix.hh>
 #include <dune/common/misc.hh>
-#include <dune/common/operator.hh>
 #include <dune/common/poolallocator.hh>
 #include <dune/common/precision.hh>
 #include <dune/common/propertymap.hh>
@@ -51,7 +46,6 @@ find dune/common/ -maxdepth 1 -name \*.hh -exec echo '#include <{}>' \; | sort
 #include <dune/common/tuples.hh>
 #include <dune/common/typeinfo.hh>
 #include <dune/common/typetraits.hh>
-#include <dune/common/vectorspace.hh>
 
 int main () {
 }
