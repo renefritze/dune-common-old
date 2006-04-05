86277d28fce87fd1142089e7b300be1ed97cc925
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-04-05 10:31:53 +0000

    minor change.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4477 419231ca-4411-0410-aba2-fc6cf8c4f609

--- grid/alu3dgrid/iterator.hh
+++ grid/alu3dgrid/iterator.hh
@@ -761,7 +761,7 @@ public:
   bool boundary () const;
 
   //! return true if across the edge an neighbor on this level exists
-  bool neighbor () const DUNE_DEPRECATED; 
+  bool neighbor () const; 
 
   //! return true if across the edge an neighbor on this level exists
   bool levelNeighbor () const; 
