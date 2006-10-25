5739c3b7624d7283d8c240d257eb5f54b6ce1330
robertk <robertk@419231ca-4411-0410-aba2-fc6cf8c4f609>
2006-10-25 13:53:10 +0000

    added CHECK_AND_CALL_INTERFACE_IMPLEMENTATION which can be used for void interface methods.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4756 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/bartonnackmanifcheck.hh
+++ common/bartonnackmanifcheck.hh
@@ -1,6 +1,3 @@
-#ifndef DUNE_BARTONNACKMANIFCHECK_HH
-#define DUNE_BARTONNACKMANIFCHECK_HH
-
 /** @file
   @author Robert Kloefkorn
   @brief Provides check for implementation of interface methods when using 
@@ -13,7 +10,25 @@
 //- Dune includes 
 #include <dune/common/exceptions.hh>
 
-namespace Dune {
+/** The macro CHECK_AND_CALL_INTERFACE_IMPLEMENTATION throws an exception,
+  if the interface method ist not implemented and just calls the method
+  otherwise. If NDEBUG is defined and DUNE_DEVEL_MODE is not defined not
+  checking is done and the method is just called.
+*/
+
+#ifndef CHECK_AND_CALL_INTERFACE_IMPLEMENTATION 
+#define CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(__interface_method_to_call__) \
+  (__interface_method_to_call__)
+#ifndef NDEBUG 
+#ifdef DUNE_DEVEL_MODE
+#undef CHECK_AND_CALL_INTERFACE_IMPLEMENTATION
+#define CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(__interface_method_to_call__) \
+  CHECK_INTERFACE_IMPLEMENTATION(__interface_method_to_call__)
+#endif
+#endif
+#endif
+
+#ifndef CHECK_INTERFACE_IMPLEMENTATION  
 #define CHECK_INTERFACE_IMPLEMENTATION(dummy) 
 #ifndef NDEBUG 
 #ifdef DUNE_DEVEL_MODE
@@ -29,5 +44,4 @@ namespace Dune {
   }
 #endif
 #endif
-} // end namespace Dune 
 #endif
