3b5a895ec41ba888e98f460dc7611d74ffc02085
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2007-07-16 09:09:46 +0000

    Fixed Size of the tuples.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@4954 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/test/tuplestest.cc
+++ common/test/tuplestest.cc
@@ -2,12 +2,25 @@
 #include "config.h"
 
 #include<dune/common/tuples.hh>
+#include<dune/common/helpertemplates.hh>
 #include<string>
 #include<iostream>
 #include<vector>
 #include<cassert>
 using namespace Dune;
 
+template<int i, int j>
+struct Same
+{
+  enum{ value = false};
+};
+
+template<int i>
+struct Same<i,i>
+{
+  enum{ value = true};
+};
+  
 template<class T>
 void test(T& tuple)
 {
@@ -37,8 +50,8 @@ int iteratorTupleTest()
   
 
   Tuple tuple(v.begin(), v.begin(), v.end());
+  IsTrue<Size<Tuple>::value==3>::yes();
 
-  assert(Size<Tuple>::value==3);
   int ret=0;
   
   if(Element<0>::get(tuple)!= v.begin()){
