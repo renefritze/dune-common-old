bcc401c1f15413270631eec3efe188788201a58b
mnolte <mnolte@419231ca-4411-0410-aba2-fc6cf8c4f609>
2008-07-22 19:25:26 +0000

    added a simple new / delete overload useful for small objects
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5247 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ common/test/smallobject.cc
@@ -0,0 +1,59 @@
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/common/timer.hh>
+#include <dune/common/smallobject.hh>
+
+using namespace Dune;
+
+class A
+{
+  int a_;
+
+public:
+  A( int a )
+  : a_( a )
+  {}
+};
+
+
+class B
+: public SmallObject
+{
+  int b_;
+
+public:
+  B( int b )
+  : b_( b )
+  {}
+};
+
+
+int main ( int argc, char **argv )
+{
+  Timer timer;
+
+  const unsigned long iterations = 1 << 30;
+  std :: cout << "Performing " << iterations << " iterations." << std :: endl;
+
+  timer.reset();
+  for( unsigned long i = 0; i < iterations; ++i )
+  {
+    A *a = new A( (int)i );
+    delete a;
+  }
+  double timeA = timer.elapsed();
+  std :: cout << "Time without SmallObject: " << timeA << std :: endl;
+
+  timer.reset();
+  for( unsigned long i = 0; i < iterations; ++i )
+  {
+    B *b = new B( (int)i );
+    delete b;
+  }
+  double timeB = timer.elapsed();
+  std :: cout << "Time with SmallObject: " << timeB << std :: endl;
+
+  std :: cout << "Result: SmallObject is " << (timeA / timeB) << " times faster." << std :: endl;
+}
