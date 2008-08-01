97ecbc99bd92307ca1e0ebc7298236abc3a104b0
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2008-08-01 23:21:19 +0000

    I was curious and added comparison to pool allocator, which seems faster.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5258 419231ca-4411-0410-aba2-fc6cf8c4f609

--- common/test/smallobject.cc
+++ common/test/smallobject.cc
@@ -4,6 +4,7 @@
 
 #include <dune/common/timer.hh>
 #include <dune/common/smallobject.hh>
+#include <dune/common/poolallocator.hh>
 
 using namespace Dune;
 
@@ -34,7 +35,7 @@ int main ( int argc, char **argv )
 {
   Timer timer;
 
-  const unsigned long iterations = 1 << 30;
+  const unsigned long iterations = 1 << 26;
   std :: cout << "Performing " << iterations << " iterations." << std :: endl;
 
   timer.reset();
@@ -51,9 +52,21 @@ int main ( int argc, char **argv )
   {
     B *b = new B( (int)i );
     delete b;
-  }
+  } 
   double timeB = timer.elapsed();
   std :: cout << "Time with SmallObject: " << timeB << std :: endl;
-
   std :: cout << "Result: SmallObject is " << (timeA / timeB) << " times faster." << std :: endl;
+
+  timer.reset();
+  PoolAllocator<B,100> pool;
+  for( unsigned long i = 0; i < iterations; ++i )
+  {
+    B *b = pool.allocate(1);
+    pool.construct(b, B((int)i ));
+    pool.destroy(b);
+    pool.deallocate(b,1);
+  }
+  double timeB2 = timer.elapsed();
+  std :: cout << "Time with pool allocator: " << timeB2 << std :: endl;
+  std :: cout << "Result: pool allocator is " << (timeA / timeB2) << " times faster." << std :: endl;
 }
