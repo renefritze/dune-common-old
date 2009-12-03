f94ae3d151d06220d26c893e05d970264b0db503
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2009-12-03 13:44:26 +0000

    New version without reinterpret casts. Please test!
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5729 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/test/poolallocatortest.cc
+++ dune/common/test/poolallocatortest.cc
@@ -32,7 +32,7 @@ struct UnAligned
     unsigned long* oelements = new unsigned long[10*elements];
     
     typedef typename Pool<T,size>::Chunk Chunk;
-        
+    
     //Fill 10 chunks
     for(int chunk=0; chunk < 10; ++chunk){
       //std::cout<< std::endl<<"Chunk "<<chunk<<" ";
@@ -67,7 +67,7 @@ struct UnAligned
 	  //  std::cout << element<<" "<<celement<<",  "<<std::endl;
 
 	  if(element< reinterpret_cast<unsigned long>(currentChunk->chunk_)){
-	    std::cerr <<" buffer overflow during first alloc: "<<reinterpret_cast<unsigned long>(currentChunk->chunk_)
+	    std::cerr <<" buffer underflow during first alloc: "<<reinterpret_cast<unsigned long>(currentChunk->chunk_)
 		      <<">"<<element<<"+"<<sizeof(T)<<std::endl;
 	    return ++ret;
 	  }
@@ -106,8 +106,8 @@ int testPool()
   std::cout<<"Checking "<<typeid(T).name()<<" sizeof="<<sizeof(T)<<" with size "<< size<<
     " alignment="<<AlignmentOf<T>::value<<std::endl;
 
-  ret += testPool<0,T>();
-  ret += testPool<size,T>();
+  //ret += testPool<0,T>();
+  //ret += testPool<size,T>();
   ret += testPool<5*size,T>();
   ret += testPool<11*size,T>();
   ret += testPool<33*size,T>();
