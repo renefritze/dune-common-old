3c4fe0a56c37ed7b122c9b7e4a25fde976495d7b
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2009-12-04 10:25:19 +0000

    Adhere to shared_ptr: empty constructor constructs null pointer.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5732 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/smartpointer.hh
+++ dune/common/smartpointer.hh
@@ -90,7 +90,7 @@ namespace Dune
             /** @brief The representative. */
             MemberType * rep_;
             /** @brief Default Constructor. */
-            PointerRep() : count_(1), rep_(new MemberType) {}
+            PointerRep() : count_(0), rep_(0) {}
             /** @brief Constructor from existing Pointer. */
             PointerRep(MemberType * p) : count_(1), rep_(p) {}
             /** @brief Destructor, deletes MemberType* rep_. */
