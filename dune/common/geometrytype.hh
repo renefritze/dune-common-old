40de9051f539eb6d992166dcd707e276ad1f864d
christi <christi@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-03-12 07:40:59 +0000

    revert accidential commit... argh\!
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5923 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/geometrytype.hh
+++ dune/common/geometrytype.hh
@@ -160,57 +160,46 @@ namespace Dune {
             return basicType_ < other.basicType_;
         }
 
-        friend inline std::ostream& operator<< (std::ostream& s, const GeometryType& a);
-    };
-
-    /** \brief Prints the type to an output stream */
-    inline std::ostream& operator<< (std::ostream& s, const GeometryType& a)
-    {
-        switch (a.basicType_) {
-            case GeometryType::simplex:   
+        /** \brief Prints the type to an output stream */
+        friend std::ostream& operator<< (std::ostream& s, const GeometryType& a)
+        {
+            switch (a.basicType_) {
+            case simplex:   
                 s << "(simplex, " << a.dim_ << ")";
                 break;
-            case GeometryType::cube:      
+            case cube:      
                 s << "(cube, " << a.dim_ << ")";
                 break;
-            case GeometryType::pyramid:
+            case pyramid:
                 s << "pyramid"; 
                 break;
-            case GeometryType::prism:
+            case prism:
                 s << "prism";
                 break;
-            case GeometryType::none:
+            case none:
                 s << "(none, " << a.dim_ << ")";
                 break;
             default:
                 s << "invalid geometry type";
+            }
+            
+            return s;
         }
-        return s;
-    }
+
+    };
 
     /** \brief Prints a GeometryType::BasicType to an output stream */
     inline std::ostream& operator<< (std::ostream& s, GeometryType::BasicType type)
     {
-        switch (type) {
-            case GeometryType::simplex:
-                s << "simplex";
-                break;
-            case GeometryType::cube:
-                s << "cube";
-                break;
-            case GeometryType::pyramid:
-                s << "pyramid";
-                break;
-            case GeometryType::prism:
-                s << "prism";
-                break;
-            case GeometryType::none:
-                s << "none";
-                break;
-            default:
-                s << "[unknown GeometryType::BasicType: " << int(type) << "]";
-        }
-        return s;
+      switch (type) {
+      case GeometryType::simplex: s << "simplex"; break;
+      case GeometryType::cube:    s << "cube";    break;
+      case GeometryType::pyramid: s << "pyramid"; break;
+      case GeometryType::prism:   s << "prism";   break;
+      case GeometryType::none:    s << "none";    break;
+      default: s << "[unknown GeometryType::BasicType: " << int(type) << "]";
+      }
+      return s;
     }
 }
 
