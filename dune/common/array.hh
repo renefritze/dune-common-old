7c3f8a5dcae551018479fc83222207ea9ec85b4f
sander <sander@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-03-15 20:45:12 +0000

    increase stl-compatibility: allow construction from brace-enclosed initializer list.  Thanks to Bard Skaflestad for the patch.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5933 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/array.hh
+++ dune/common/array.hh
@@ -63,9 +63,6 @@ namespace Dune
       /** \brief Const reverse iterator type */
       typedef std::reverse_iterator<const_iterator>   const_reverse_iterator;
 
-      //! Create uninitialized array
-      array () {}
-      
       /** \brief Return array size */
       size_type size() const {return N;}
 
@@ -114,7 +111,6 @@ namespace Dune
         return a + N;
       }
 
-  protected:
       T a[(N > 0) ? N : 1];
   };
 
