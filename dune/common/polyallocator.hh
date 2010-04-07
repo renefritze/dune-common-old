eae348b13e6a051521ad3477ed5531507728b162
mnolte <mnolte@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-04-07 07:36:52 +0000

    add an allocator that can create object of arbitrary type (otherwise similar to STL)
    This will need some further discussion, of course. It is intended for FS #766,
    i.e., use an allocator to construct polymorphic objects in GenericGeometries.
    
    
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5954 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ dune/common/polyallocator.hh
@@ -0,0 +1,36 @@
+#ifndef DUNE_POLYALLOCATOR_HH
+#define DUNE_POLYALLOCATOR_HH
+
+namespace Dune
+{
+
+  // PolyAllocator
+  // -------------
+
+  struct PolyAllocator
+  {
+    template< class T >
+    T *allocate ( size_t n = 1 )
+    {
+      return static_cast< T * >( operator new( n * sizeof( T ) ) );
+    }
+
+    template< class T > void deallocate ( T *p )
+    {
+      operator delete( p );
+    }
+
+    template< class T > void construct ( T *p, const T &value )
+    {
+      new( p ) T( value );
+    }
+
+    template< class T > void destroy ( T *p )
+    {
+      p->~T();
+    }
+  };
+
+}
+
+#endif // #ifndef DUNE_POLYALLOCATOR_HH
