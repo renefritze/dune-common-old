d01e9d42c1b987c2b2bca6f806901a12487cc066
mnolte <mnolte@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-04-07 12:20:45 +0000

    added test showing what polyallocator shall be able to do
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@5956 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ dune/common/test/polyallocator.cc
@@ -0,0 +1,66 @@
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/common/polyallocator.hh>
+
+struct A
+{
+  virtual ~A () {}
+  virtual void test ( int i ) = 0;
+};
+
+
+struct B
+: public A
+{
+  B( int i ) : k( i ) {}
+
+  void test ( int i )
+  {
+    std::cout << "B( " << k << " ).test( " << i << " )" << std::endl;
+  }
+
+  int k;
+};
+
+
+struct C
+: public A
+{
+  void test ( int i )
+  {
+    std::cout << "C.test( " << i << " )" << std::endl;
+  }
+};
+
+
+A *create ( int argc, char **argv, Dune::PolyAllocator &allocator )
+{
+  if( argc > 1 )
+  {
+    B *b = allocator.allocate< B >();
+    allocator.construct( b, B( atoi( argv[ 1 ] ) ) );
+    return b;
+  }
+  else
+  {
+    C *c = allocator.allocate< C >();
+    allocator.construct( c, C() );
+    return c;
+  }
+}
+
+
+int main ( int argc, char **argv )
+{
+  Dune::PolyAllocator allocator;
+  A *a = create( argc, argv, allocator );
+
+  a->test( 2 );
+
+  allocator.destroy( a );
+  allocator.deallocate( a );
+
+  return 0;
+}
