6844dcac0d52fcbae72e6191a67271f12418c3ef
sander <sander@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-08-09 09:38:36 +0000

    spelling
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@6091 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/mpihelper.hh
+++ dune/common/mpihelper.hh
@@ -66,8 +66,8 @@ namespace Dune
   public:
     enum{ 
       /**
-       * @brief Are we fake (i. e. pretend to have MPI support but are compiled
-       * without.
+       * @brief Are we fake (i.e. pretend to have MPI support but are compiled
+       * without.)
        */
       isFake = true
     };
