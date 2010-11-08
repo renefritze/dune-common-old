a4a6ca402fdbe34afeb2a5ba4a29fa8114116187
sander <sander@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-11-08 15:18:19 +0000

    free function to pretty-print the class name of an object.  Moved here from fvectortest.cc
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@6239 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ dune/common/classname.hh
@@ -0,0 +1,28 @@
+#ifndef DUNE_CLASSNAME_HH
+#define DUNE_CLASSNAME_HH
+
+/** \file
+ * \brief A free function to provide the demangled class name 
+ *        of a given object as a string
+ */
+
+#ifdef __GNUC__
+#include <cxxabi.h>
+#endif
+
+namespace Dune {
+ 
+    /** \brief Provide the demangled class name of a given object as a string */
+    template <class T>
+    std::string className(T &t)
+    {
+#ifdef __GNUC__
+        int status;
+        return abi::__cxa_demangle(typeid(t).name(),0,0,&status);
+#else
+        return typeid(t).name();
+#endif
+    };
+
+}
+#endif  // DUNE_CLASSNAME_HH
