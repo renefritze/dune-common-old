86f41c994d7ec042a7bf770b0d955dca242ca474
joe <joe@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-12-02 00:02:34 +0000

    [FirstTypeIndex, FirstPredicateIndex] Classes to find the index of a particular type in a tuple.
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@6289 419231ca-4411-0410-aba2-fc6cf8c4f609

---
+++ dune/common/test/tupleutilitytest.cc
@@ -0,0 +1,30 @@
+// -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set ts=8 sw=2 et sts=2:
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <cstddef>
+
+#include <dune/common/static_assert.hh>
+#include <dune/common/tuples.hh>
+#include <dune/common/tupleutility.hh>
+
+//////////////////////////////////////////////////////////////////////
+//
+// check FirstTypeIndex
+//
+typedef Dune::tuple<int, unsigned, double> MyTuple;
+dune_static_assert((Dune::FirstTypeIndex<MyTuple, int>::value == 0),
+                   "FirstTypeIndex finds the wrong index for double in "
+                   "MyTuple!");
+dune_static_assert((Dune::FirstTypeIndex<MyTuple, unsigned>::value == 1),
+                   "FirstTypeIndex finds the wrong index for double in "
+                   "MyTuple!");
+dune_static_assert((Dune::FirstTypeIndex<MyTuple, double>::value == 2),
+                   "FirstTypeIndex finds the wrong index for double in "
+                   "MyTuple!");
+
+int main() {
+}
