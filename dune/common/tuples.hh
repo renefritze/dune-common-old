8ab1693de83745a961d6256d6dd8ef380c894daf
mnolte <mnolte@419231ca-4411-0410-aba2-fc6cf8c4f609>
2010-12-08 08:37:57 +0000

    input for tuples
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@6297 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/tuples.hh
+++ dune/common/tuples.hh
@@ -4,7 +4,8 @@
 #ifndef DUNE_TUPLES_HH
 #define DUNE_TUPLES_HH
 
-#include<ostream>
+#include<iostream>
+
 #include"typetraits.hh"
 #include"static_assert.hh"
 
@@ -615,7 +616,21 @@ struct ElementType
     template<class T>
     static std::ostream& put(std::ostream& os, const T& t, const char* delim=", ")
     {
-      return tuple_writer<i-1>::put(os,t,delim)<<delim<<get<i-1>(t);
+      return tuple_writer<i-1>::put(os,t,delim)<<delim<<Dune::get<i-1>(t);
+    }
+
+    template< class T >
+    static std::istream &get ( std::istream &is, T &t, const char *delim = "," )
+    {
+      tuple_writer< i-1 >::get( is, t, delim );
+      for( const char *it = delim; is && (*it != 0); ++it )
+      {
+        char c = 0;
+        is >> c;
+        if( c != *it )
+          is.setstate( std::ios::failbit );
+      }
+      return is >> Dune::get< i-1 >( t );
     }
   };
 
@@ -625,7 +640,13 @@ struct ElementType
     template<class T>
     static std::ostream& put(std::ostream& os, const T& t, const char* delim=", ")
     {
-      return os<<get<0>(t);
+      return os<<Dune::get<0>(t);
+    }
+
+    template< class T >
+    static std::istream &get ( std::istream &is, T &t, const char *delim = ", " )
+    {
+      return is >> Dune::get< 0 >( t );
     }
    };
   
@@ -636,6 +657,12 @@ struct ElementType
     static std::ostream& put(std::ostream& os, const T& t, const char* delim=", ")
     {
       return os;
+    }
+
+    template< class T >
+    static std::istream &get ( std::istream &is, T &t, const char *delim = ", " )
+    {
+      return is;
     }
   };
 
@@ -716,6 +743,81 @@ struct ElementType
     return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
   }
 
+  /** 
+   * \brief Read a tuple.
+   */
+  template<typename T1>
+  inline std::istream& operator>>( std::istream& is, tuple<T1> & t)
+  {
+    typedef tuple<T1> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2> & t)
+  {
+    typedef tuple<T1,T2> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3> & t)
+  {
+    typedef tuple<T1,T2,T3> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4> & t)
+  {
+    typedef tuple<T1,T2,T3,T4> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5,T6> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5,T6,T7> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
+	   typename T8>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
+	   typename T8, typename T9>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
+  
+  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
+           typename T8, typename T9, typename T10>
+  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> & t)
+  {
+    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> TupleT;
+    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+  }
 #else
   /**
    * @brief Equality comparison operator for tuples.
@@ -849,7 +951,7 @@ struct ElementType
   }
 
   /**
-   * @brief Print aa pair or  tuple.
+   * @brief Print a pair or tuple.
    */
   template<typename T1, typename T2>
   inline std::ostream& operator<<(std::ostream& os, const Pair<T1,T2>& pair)
@@ -863,6 +965,21 @@ struct ElementType
   {
     os<<pair.first();
     return os;
+  }
+
+  /**
+   * @brief Read a pair or tuple.
+   */
+  template<typename T1, typename T2>
+  inline std::istream& operator>>(std::istream& is, Pair<T1,T2>& pair)
+  {
+    return is >> pair.first() >> pair.second();
+  }
+
+  template<typename T1>
+  inline std::istream& operator>>(std::istream& is, Pair<T1,Nil>& pair)
+  {
+    return is >> pair.first();
   }
 
   template<class T1>
