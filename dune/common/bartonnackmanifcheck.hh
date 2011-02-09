7e1f6200c5fd4061f50a15c927bbc4d636b2dcf5
christi <christi@419231ca-4411-0410-aba2-fc6cf8c4f609>
2011-02-09 20:28:21 +0000

    - get rid DUNE_DEVEL_MODE - if NDEBUG is not set, CHECK_INTERFACE_IMPLEMENTATION is active - add proper double-inclusion-protection
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@6331 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/bartonnackmanifcheck.hh
+++ dune/common/bartonnackmanifcheck.hh
@@ -1,3 +1,6 @@
+#ifndef DUNE_COMMON_BACTONNACKMANIFCHECK_HH
+#define DUNE_COMMON_BACTONNACKMANIFCHECK_HH
+
 /** @file
   @author Robert Kloefkorn
   @brief Provides check for implementation of interface methods when using 
@@ -10,12 +13,9 @@
 //- Dune includes 
 #include <dune/common/exceptions.hh>
 
-#ifndef CHECK_INTERFACE_IMPLEMENTATION  
+#ifdef NDEBUG 
 #define CHECK_INTERFACE_IMPLEMENTATION(dummy) 
-#ifndef NDEBUG 
-// only do checking if DUNE_DEVEL_MODE is defined aswell 
-#ifdef DUNE_DEVEL_MODE
-#undef CHECK_INTERFACE_IMPLEMENTATION
+#else
 #define CHECK_INTERFACE_IMPLEMENTATION(__interface_method_to_call__) \
   {\
     static bool call = false; \
@@ -33,24 +33,18 @@
     } \
   }
 #endif
-#endif
-#endif
 
 /** The macro CHECK_AND_CALL_INTERFACE_IMPLEMENTATION throws an exception,
   if the interface method ist not implemented and just calls the method
   otherwise. If NDEBUG is defined no
   checking is done and the method is just called.
 */
-#ifndef CHECK_AND_CALL_INTERFACE_IMPLEMENTATION 
+#ifndef NDEBUG 
 #define CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(__interface_method_to_call__) \
   (__interface_method_to_call__)
-#ifndef NDEBUG 
-// only do checking if DUNE_DEVEL_MODE is defined aswell 
-#ifdef DUNE_DEVEL_MODE
-#undef CHECK_AND_CALL_INTERFACE_IMPLEMENTATION
+#else
 #define CHECK_AND_CALL_INTERFACE_IMPLEMENTATION(__interface_method_to_call__) \
   CHECK_INTERFACE_IMPLEMENTATION(__interface_method_to_call__)
 #endif
-#endif
-#endif
 
+#endif // DUNE_COMMON_BACTONNACKMANIFCHECK_HH
