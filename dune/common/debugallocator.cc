2725266c705f61cb248e7f7dbd627c0b5593d7a5
christi <christi@419231ca-4411-0410-aba2-fc6cf8c4f609>
2012-10-05 09:11:45 +0000

    [debugallocator] rename files... remove underscores
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@7012 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/debugallocator.cc
+++ dune/common/debugallocator.cc
@@ -2,7 +2,7 @@
 #include "config.h"
 #endif
 
-#include "debug_allocator.hh"
+#include "debugallocator.hh"
 
 #include <iostream>
 #include <unistd.h>
--- dune/common/test/testdebugallocator.cc
+++ dune/common/test/testdebugallocator.cc
@@ -5,7 +5,7 @@
 // #define DEBUG_ALLOCATOR_KEEP 1
 #define DEBUG_NEW_DELETE 3
 
-#include <dune/common/debug_allocator.hh>
+#include <dune/common/debugallocator.hh>
 
 #include <iostream>
 #include <cstdlib>
