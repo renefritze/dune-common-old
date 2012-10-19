255efcf6d5af8eb4d46f51534810294084f37d53
mblatt <mblatt@419231ca-4411-0410-aba2-fc6cf8c4f609>
2012-10-19 15:58:21 +0000

    Merged patch from cmake branch. (Support type conversion for shared_ptr. Needed for gcc-4.1.)
    git-svn-id: https://svn.dune-project.org/svn/dune-common/trunk@7043 419231ca-4411-0410-aba2-fc6cf8c4f609

--- dune/common/test/shared_ptrtest.cc
+++ dune/common/test/shared_ptrtest.cc
@@ -14,6 +14,7 @@
 
 #if defined(DISABLE_CONFIGURED_SHARED_PTR) && defined(SHARED_PTR_NAMESPACE)
 #undef SHARED_PTR_NAMESPACE
+#undef HAVE_MAKE_SHARED
 #endif
 
 #include<dune/common/classname.hh>
@@ -47,6 +48,13 @@ class A {};
 class B : public A {};
 class C : A {};
 
+
+Dune::shared_ptr<A> test_make_shared()
+{
+    return Dune::make_shared<B>();
+}
+
+
 int main(){
     using namespace Dune;
     int ret=0;
@@ -54,6 +62,9 @@ int main(){
         // test default constructor
         shared_ptr<int> foo;
 
+        // test conversion in make_shared
+        shared_ptr<A> a=test_make_shared();
+        
         // print the type of the shared_ptr, so we know whether we are
         // checking dune's shared_ptr or some std one
         std::cout << "=== checking shared_ptr type: " << className(foo)
